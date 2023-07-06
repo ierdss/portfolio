1995c80437df802e43a8e266225d0fa183c41d6e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-07 05:59:30 +0800

    Update Hero.tsx
    -Removed white border

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
     const sizeIcon = 30;
 
     return(
-        <main id="home" className="w-full py-20">
+        <main id="home" className="w-full py-20 bg-slate-50">
             <div className="flex flex-wrap flex-col md:flex-row justify-center items-center w-full md:w-fit h-1/2 md:h-[700px] py-20 px-6 md:px-20 gap-8">
-                <div className="w-[300px] md:w-[400px] aspect-square rounded-full p-6 bg-slate-400 border-4 border-white">
+                <div className="w-[300px] md:w-[400px] aspect-square rounded-full p-6 bg-slate-400 border-white">
                 </div>
                 <div className="flex flex-col w-full md:w-1/2 gap-4 text-center justify-center items-center md:text-left md:items-start">
                     <div className='flex flex-row w-fit justify-center items-center gap-4 md:order-3'>
