6919d070a9adda612b8f13eb8538235f1335adc5
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-04 19:01:17 +0800

    Updated Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
+import { useEffect } from 'react'
 import { NavLinks } from '@/constants'
-import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi'
@@ -14,19 +14,32 @@ export default function Navbar() {
         setNav(!nav);
     }
 
+    useEffect(() => {
+        const onScroll = () => {
+            const navbar = document.getElementById("navbar")!;
+            const distance = 50;
+            if (document.documentElement.scrollTop > distance) {
+                navbar.classList.add("navbar__black");
+            } else {
+                navbar.classList.remove("navbar__black");
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+    }, []);
+
     return(
-        <nav className='flexCenter py-4 px-6 md:px-12 w-full fixed top-0 bg-white !z-50 max-w-screen-xl'>
+        <nav id='navbar' className='flexCenter py-4 px-6 md:px-12 w-full fixed top-0 !z-50 max-w-screen-xl bg-transparent text-slate-200'>
             <div className='flex-1 flexBetween gap-10 justify-center items-center'>
                 <Link href={"/"}>
                     <h1 className='text-5xl font-black bg-clip-text wk-bg-clip text-transparent bg-cover bg-center bg-home-texture hover:text-secondary-red'>ANDREI</h1>
                 </Link>
                 <div className='lg:flex hidden gap-10'>
-                    <ul className='flex flex-row gap-2'>
+                    <ul id='navbar-links' className='flex flex-row gap-2'>
                         {NavLinks.map(({ id, href, text}) => (
                             <li key={id}>
                             <Link
                                 href={href} 
-                                className='p-4 font-bold text-lg text-slate-400 hover:text-secondary-red target:text-secondary-red'
+                                className='p-4 font-bold text-lg hover:text-secondary-red target:text-secondary-red'
                                 >
                                 {text}
                             </Link>
@@ -39,14 +52,14 @@ export default function Navbar() {
                 {!nav ? (<GiHamburgerMenu size={sizeIcon}/>) : (<RxCross1 size={sizeIcon}/>)}
             </div>
             <div className={!nav ? 'hidden' : 'block absolute top-20 w-full bg-white'} >
-                <ul className='flex flex-col w-full pb-8'>
+                <ul id='navbar-links' className='flex flex-col w-full pb-8 text-slate-400'>
                     {NavLinks.map((link) => (
                         <li key={link.key}
                             className='w-full'>
                             <Link
                                 onClick={handleNav}
                                 href={link.href} 
-                                className='block w-full p-6 font-bold text-lg text-slate-400 hover:text-secondary-red'
+                                className='block w-full p-6 font-bold text-lg  hover:text-secondary-red'
                                 >
                                 {link.text}
                             </Link>
