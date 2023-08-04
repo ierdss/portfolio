0551e9bd5a5aa7b5d090f934879f981d9d6a83f2
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-04 12:37:14 +0800

    Update Links.tsx

--- components/Links.tsx
+++ components/Links.tsx
@@ -7,14 +7,12 @@ type classNameProps = {
 
 export default function Links({ className }:classNameProps) {
     return(
-        <div className={className}>
-            <ul className='flex flex-row justify-center items-center gap-4'>
-                {SocialMediaLinks.map(({ id, icon, url}) => (
-                    <li key={id}>
-                        <Link href={url} target='_blank' className='text-secondary-red'>{icon}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <ul className={className || 'flex flex-row justify-center items-center gap-4'}>
+            {SocialMediaLinks.map(({ id, icon, url}) => (
+                <li key={id}>
+                    <Link href={url} target='_blank' className='text-secondary-red'>{icon}</Link>
+                </li>
+            ))}
+        </ul>
     )
 }
