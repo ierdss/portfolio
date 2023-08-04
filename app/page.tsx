966733d9182f2daf32349dbc06c39c9e035509ac
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-04 17:45:15 +0800

    Added 3d model

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import About from '@/components/About'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
+import CanvasTesting from '@/components/Testing'
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full">
+      <CanvasTesting/>
       <Hero/>
       <About/>
       <Projects/>
