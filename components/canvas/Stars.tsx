7402ba1293d8a9ab9a7d51c11ff1e68faa65069b
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-04 19:49:29 +0800

    Checkpoint

--- components/canvas/Stars.tsx
+++ components/canvas/Stars.tsx
@@ -1,20 +1,27 @@
-import { useState, useRef, Suspense } from 'react'
+import React, { useState, useRef, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
-import * as random from "maath/random/dist/maath-random.esm";
+import * as random from "maath/random";
 
-const Stars = () => {
-    const ref = useRef();
+type StarsProps = {
+
+}
+
+const Stars = ({ props }:) => {
+    const ref = useRef<THREE.Object3D>();
     const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
     useFrame((state, delta) => {
-      ref.current.rotation.x += delta / 1!;
-      ref.current.rotation.y += delta / 15;
+        if (ref.current) {
+            ref.current.rotation.x += delta / 1!;
+            ref.current.rotation.y += delta / 15;
+        }
     });
+
 
     return(
         <group rotation={[0, 0, Math.PI / 4]}>
-            <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+            <Points ref={ref} positions={sphere} stride={3} frustumCulled>
             <PointMaterial
                 transparent
                 color='#f272c8'
