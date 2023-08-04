e8979b23cd24b8018db9bb65ad543ea854640ce2
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-04 17:45:15 +0800

    Added 3d model

---
+++ components/Testing.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import EarthCanvas from "./canvas/Earth";
+
+export default function CanvasTesting() {
+    return(
+        <div className="pt-28 w-full h-[900px] bg-black">
+            <EarthCanvas/>
+        </div>
+    )
+}
---
+++ components/canvas/Earth.tsx
@@ -0,0 +1,47 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+
+import CanvasLoader from "../Loader"
+
+const Earth = () => {
+    const earth = useGLTF('./planet/scene.gltf');
+
+    return(
+        <primitive 
+            object={earth.scene}
+            scale={2.5}
+            position-y={0}
+            rotation-y={0}
+        />
+    )
+}
+
+export default function EarthCanvas() {
+    return(
+        <Canvas
+            shadows
+            frameloop="demand"
+            gl={{ preserveDrawingBuffer: true }}
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 100,
+                position: [-4, 3, 6],
+            }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    autoRotate
+                    autoRotateSpeed={10}
+                    rotateSpeed={2}
+                    enableZoom={false}
+                    minPolarAngle={Math.PI / 2}
+                    maxPolarAngle={Math.PI / 2}
+                />
+                <Earth />
+                <Preload all />
+            </Suspense>
+        </Canvas>
+    )
+}
