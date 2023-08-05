c0682a5f2b2cba2cd831a0a7e7e1c03886f4c282
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 19:12:15 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import SocialMediaLinks from "./Links";
 
 export default function Footer() {
     return(
-        <footer className="flex flex-col w-full py-8 px-16 border-t border-nav-border">
+        <footer className="flex flex-col w-full py-6 px-16 border-t border-nav-border">
             <div className="flex-col text-center sm:flex-row sm:text-left flexBetween footer_copyright gap-8">
                 <p>@ 2023 Andrei Sager. All rights reserved</p>
                 <SocialMediaLinks/>
