1b1d85ea7a20b6510d3717bc1b7bb9b5839f7062
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 14:31:13 +0800

    Replaced Hero.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,16 @@ import About from '@/components/About'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
-import CanvasTesting from '@/components/Testing'
+import CanvasTesting from '@/components/Hero'
 import StarsCanvas from '@/components/canvas/Stars'
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full">
       <div className='relative z-0'>
-        <CanvasTesting/>
+        <Hero/>
         <StarsCanvas/>
       </div>
-      <Hero/>
       <About/>
       <Projects/>
       <Contact/>
