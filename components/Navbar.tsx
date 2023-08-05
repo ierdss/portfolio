a8deb072951de439282c834fbfa6ab23c22a76cd
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 21:04:51 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,7 @@ import { RxCross1 } from 'react-icons/rx'
 
 export default function Navbar() {
     const sizeIcon = 30;
+
     const [nav, setNav] = useState<true | false>(false);
     const handleNav = () => {
         setNav(!nav);
@@ -28,45 +29,45 @@ export default function Navbar() {
     }, []);
 
     return(
-        <nav id='navbar' className='flexCenter py-4 px-6 md:px-12 w-full fixed top-0 !z-50 max-w-screen-xl bg-transparent text-slate-200 shadow-lg'>
-            <div className='flex-1 flexBetween gap-10 md:gap-16 justify-center items-center'>
-                <Link href={"/"}>
-                    <h1 className='text-5xl font-black bg-clip-text wk-bg-clip text-transparent bg-cover bg-center bg-home-texture hover:text-secondary-red'>ANDREI</h1>
-                </Link>
-                <div className='lg:flex hidden gap-10'>
-                    <ul id='navbar-links' className='flex flex-row gap-2'>
-                        {NavLinks.map(({ id, href, text}) => (
-                            <li key={id}>
-                            <Link
-                                href={href} 
-                                className='p-4 font-bold text-lg hover:text-secondary-red target:text-secondary-red'
-                                >
-                                {text}
-                            </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-            <div onClick={handleNav} className='lg:hidden flex flex-row gap-10'>
-                {!nav ? (<GiHamburgerMenu size={sizeIcon}/>) : (<RxCross1 size={sizeIcon}/>)}
-            </div>
-            <div className={!nav ? 'hidden' : 'block absolute top-20 w-full bg-white'} >
-                <ul id='navbar-links' className='flex flex-col w-full pb-8 text-slate-400'>
-                    {NavLinks.map((link) => (
-                        <li key={link.key}
-                            className='w-full'>
-                            <Link
-                                onClick={handleNav}
-                                href={link.href} 
-                                className='block w-full p-6 font-bold text-lg  hover:text-secondary-red'
-                                >
-                                {link.text}
-                            </Link>
+    <nav id='navbar' className='flexCenter  py-4 px-6 md:px-12 w-full fixed top-0 !z-40 max-w-screen-xl bg-transparent text-slate-200 shadow-lg'>
+        <div className='flex-1 flexBetween gap-10 md:gap-16 justify-center items-center'>
+            <Link href={"/"}>
+                <h1 className='text-5xl font-black bg-clip-text wk-bg-clip text-transparent bg-cover bg-center bg-home-texture hover:text-secondary-red'>ANDREI</h1>
+            </Link>
+            <div className='lg:flex hidden gap-10'>
+                <ul id='navbar-links' className='flex flex-row gap-2'>
+                    {NavLinks.map(({ id, href, text}) => (
+                        <li key={id}>
+                        <Link
+                            href={href} 
+                            className='p-4 font-bold text-lg hover:text-secondary-red target:text-secondary-red'
+                            >
+                            {text}
+                        </Link>
                         </li>
                     ))}
                 </ul>
             </div>
-        </nav>
+        </div>
+        <div onClick={handleNav} className='lg:hidden flex flex-row gap-10'>
+            {!nav ? (<GiHamburgerMenu size={sizeIcon}/>) : (<RxCross1 size={sizeIcon}/>)}
+        </div>
+        <div className={!nav ? 'hidden' : 'block absolute top-20 w-full bg-white'} >
+            <ul id='navbar-links' className='flex flex-col w-full pb-8 text-slate-400'>
+                {NavLinks.map((link) => (
+                    <li key={link.key}
+                        className='w-full'>
+                        <Link
+                            onClick={handleNav}
+                            href={link.href} 
+                            className='block w-full p-6 font-bold text-lg  hover:text-secondary-red'
+                            >
+                            {link.text}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
     )
 }
