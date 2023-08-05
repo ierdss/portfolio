1e71bf0204ead40ba809907870d2423600447777
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 14:57:19 +0800

    Improved index imports

---
+++ components/canvas/index.ts
@@ -0,0 +1,7 @@
+import EarthCanvas from "./Earth";
+import StarsCanvas from "./Stars";
+
+export {
+    EarthCanvas,
+    StarsCanvas,
+};
---
+++ components/index.ts
@@ -0,0 +1,18 @@
+import { EarthCanvas, StarsCanvas } from './canvas';
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
+export {
+  Navbar,
+  Hero,
+  About,
+  Projects,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  StarsCanvas, 
+};
