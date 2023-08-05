f108904593af692b8e4671a493baabfcfbb86209
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 19:09:00 +0800

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -17,7 +17,7 @@ export default function Contact() {
                 <div className='flex flex-col justify-between lg:flex-row gap-8'>
                     <div className="flex flex-col gap-10 w-full md:w-[300px] items-center md:items-start">
                         {ContactInfo.map(({key, title, icon, text}) => (
-                            <div key={key} className='flex flex-row items-center gap-4 p-8 border border-solid border-slate-200 rounded-tr-xl rounded-bl-xl bg-slate-100 hover:scale-105 transition duration-150 ease-in-out shadow-xl w-full max-w-[300px]'>
+                            <div key={key} className='flex flex-row items-center gap-4 p-8 border border-solid border-slate-200 rounded-tr-xl rounded-bl-xl bg-slate-100 hover:scale-105 transition duration-150 ease-in-out shadow-xl w-full max-w-[300px] group relative overflow-hidden bg-gray-200 text-sm font-medium text-slate-800 focus:outline-none focus:ring hover:border-3'>
                                 <span className='text-secondary-red'>
                                     {icon}
                                 </span>
@@ -25,6 +25,8 @@ export default function Contact() {
                                     <h4 className='font-bold text-left'>{title}</h4>
                                     <p className=' text-left'>{text}</p>
                                 </div>
+                                <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-secondary-red transition-all duration-700 group-hover:h-full"></span>
+                                <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-secondary-red transition-all duration-700 group-hover:h-full"></span>
                             </div>
                         ))}
                     </div>
