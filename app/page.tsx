8fdc8b195794faba1c4398142b912c2e585419fb
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 14:43:38 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import About from '@/components/About'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
-import CanvasTesting from '@/components/Hero'
 import StarsCanvas from '@/components/canvas/Stars'
 
 export default function Home() {
