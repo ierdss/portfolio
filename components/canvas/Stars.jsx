7d157c944d300cebfb1a197a76f06306f46f3ef3
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-05 14:18:16 +0800

    Could not fix errors

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -1,27 +1,22 @@
-import React, { useState, useRef, Suspense } from 'react'
+"use client"
+
+import { useState, useRef, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
 import * as random from "maath/random";
 
-type StarsProps = {
-
-}
-
-const Stars = ({ props }:) => {
-    const ref = useRef<THREE.Object3D>();
+const Stars = ({ props }) => {
+    const ref = useRef();
     const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
     useFrame((state, delta) => {
-        if (ref.current) {
-            ref.current.rotation.x += delta / 1!;
-            ref.current.rotation.y += delta / 15;
-        }
+        ref.current.rotation.x += delta / 1;
+        ref.current.rotation.y += delta / 15;
     });
-
 
     return(
         <group rotation={[0, 0, Math.PI / 4]}>
-            <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+            <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
             <PointMaterial
                 transparent
                 color='#f272c8'
