8e0fc3d892bccc5755c1c3133fd03dd026abcabc
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-09 17:14:29 +0800

    Update index.tsx
    +Fixed import error

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,7 +1,40 @@
-import { NavigationLinks, SectionContent, SectionHeading } from './SectionHeading'
+import { 
+    NavLinks, 
+    SocialMediaLinks, 
+} from './NavigationLinks'
+
+import { 
+    AboutSectionContent, 
+    TechnologiesSectionContent, 
+    ProjectSectionContent,
+    ContactSectionContent, 
+} from './SectionContent'
+
+import { 
+    HeroSectionHeading, 
+    AboutSectionHeading, 
+    TechnologiesSectionHeading,
+    WorkExperienceSectionHeading,
+    ProjectsSectionHeading,
+    TestimonialsSectionHeading,
+    ContactSectionHeading,
+    FooterSectionHeading, 
+} from './SectionHeading'
+
 
 export {
-    NavigationLinks,
-    SectionContent,
-    SectionHeading,
+    NavLinks, 
+    SocialMediaLinks, 
+    AboutSectionContent, 
+    TechnologiesSectionContent, 
+    ProjectSectionContent,
+    ContactSectionContent, 
+    HeroSectionHeading, 
+    AboutSectionHeading, 
+    TechnologiesSectionHeading,
+    WorkExperienceSectionHeading,
+    ProjectsSectionHeading,
+    TestimonialsSectionHeading,
+    ContactSectionHeading,
+    FooterSectionHeading, 
 };
