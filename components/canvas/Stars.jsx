2e327eb2d82a64920fd9fc502d4c5fb1f864c6f7
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-11 04:08:14 +0800

    Updated Hero.tsx

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -10,7 +10,7 @@ const Stars = ({ props }) => {
     const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
     useFrame((state, delta) => {
-        ref.current.rotation.x += delta / 1;
+        ref.current.rotation.x -= delta / 1;
         ref.current.rotation.y += delta / 15;
     });
 
