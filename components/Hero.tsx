b8fa0d1bac7cca90bf4455cbd131eca05fbc317e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-11 03:09:31 +0800

    Update Hero.tsx
    +Made heading into a card

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,16 +8,16 @@ export default function Hero() {
     const { header, subheader } = HeroSectionHeading;
 
     return(
-        <div className="sectionPadding md:!px-0 flex flex-col md:flex-row justify-center items-center gap-12 w-full h-[100vh] max-h-[900px] bg-neutral-900 overflow-hidden">
+        <div className="sectionPadding md:!px-0 py-40 flex flex-col md:flex-row justify-center items-center gap-12 w-full h-[100vh] max-h-[1000px] bg-neutral-900 overflow-hidden">
             <div className="w-full md:w-[40%] aspect-square">
                 <EarthCanvas/>
             </div>
-            <div className="flex flex-col w-full md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start text-white">
+            <div className="flex flex-col w-full md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start bg-white p-8 rounded-2xl border-r-4 border-r-secondary-red hover:scale-105 shadow-lg">
                 <div  className='flex flex-row w-fit justify-center items-center gap-4 md:order-3'>
                     <Links/>
                 </div>
-                <h1 className="font-bold text-5xl md:order-1">{header}</h1>
-                <p className="md:order-2 font-bold text-slate-300">{subheader}</p>
+                <h1 className="font-bold text-3xl md:order-1">{header}</h1>
+                <p className="md:order-2 font-bold text-slate-700">{subheader}</p>
             </div>
         </div>
     )
