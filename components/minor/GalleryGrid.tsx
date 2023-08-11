af9a8196f4ed4589525a370b5a85a680063630ba
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-12 00:05:29 +0800

    Updated GalleryGrid.tsx
    +Made gallery fully flexible

--- components/minor/GalleryGrid.tsx
+++ components/minor/GalleryGrid.tsx
@@ -1,13 +1,13 @@
 export default function GalleryGrid() {
     return(
-        <div className="w-full grid grid-col-3 gap-4">
-            <div className="w-full aspect-square bg-black rounded-xl"></div>
-            <div className="w-full aspect-square bg-black rounded-xl col-start-2 col-span-2 row-span-2"></div>
-            <div className="w-full bg-black rounded-xl row-start-2 row-end-4"></div>
-            <div className="w-full aspect-square bg-black rounded-xl"></div>
-            <div className="w-full aspect-square bg-black rounded-xl"></div>
-            <div className="w-full bg-black rounded-xl col-start-1 col-span-2"></div>
-            <div className="w-full aspect-square bg-black rounded-xl"></div>
+        <div className="galleryGrid">
+            <div className="gridSquare gridSquare__small"></div>
+            <div className="gridSquare gridSquare__big"></div>
+            <div className="gridSquare gridSquare__tall"></div>
+            <div className="gridSquare gridSquare__small"></div>
+            <div className="gridSquare gridSquare__small"></div>
+            <div className="gridSquare gridSquare__long"></div>
+            <div className="gridSquare gridSquare__small"></div>
         </div>
     )
 }
