6775cea6b5dabf8d670db53b3595dfe1a29dec6f
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-12 00:05:41 +0800

    Update GalleryGrid.tsx

--- components/minor/GalleryGrid.tsx
+++ components/minor/GalleryGrid.tsx
@@ -1,6 +1,6 @@
 export default function GalleryGrid() {
     return(
-        <div className="galleryGrid">
+        <div className="w-full grid grid-cols-3 grid-rows-4 grid-flow-dense gap-4">
             <div className="gridSquare gridSquare__small"></div>
             <div className="gridSquare gridSquare__big"></div>
             <div className="gridSquare gridSquare__tall"></div>
