9701c5046485a9c750b88a210524373a268d5f97
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-12 00:44:45 +0800

    Added Image class
    +Add images later

--- components/minor/GalleryGrid.tsx
+++ components/minor/GalleryGrid.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+
 export default function GalleryGrid() {
     return(
         <div className="w-full grid grid-cols-3 grid-rows-4 grid-flow-dense gap-4">
-            <div className="gridSquare gridSquare__small"></div>
-            <div className="gridSquare gridSquare__big"></div>
+            <div className="gridSquare gridSquare__small">
+   
+            </div>
+            <div className="gridSquare gridSquare__big">
+                {/* <Image
+                    src="/MeRed.png"
+                    width={300}
+                    height={300}
+                    alt="Me"
+                    className="gridImage"
+                /> */}
+            </div>
             <div className="gridSquare gridSquare__tall"></div>
             <div className="gridSquare gridSquare__small"></div>
             <div className="gridSquare gridSquare__small"></div>
