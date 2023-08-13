a7025501690f267b2e07821a2b552e813eed9b98
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-13 21:53:58 +0800

    Updated Names

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import Links from "./minor/Links";
 import { HeroSectionHeading} from "@/constants/SectionHeading";
-import GalleryGrid from "./minor/GalleryGrid";
+import HeroGrid from "./minor/HeroGrid";
 
 export default function Hero() {
     const { header, subheader } = HeroSectionHeading;
@@ -10,7 +10,7 @@ export default function Hero() {
     return(
         <div className="sectionPadding md:!pt-32 flex flex-col md:flex-row justify-center items-center gap-12 w-full overflow-hidden">
             <div className="w-[80%] lg:w-[40%] z-10">
-                <GalleryGrid />
+                <HeroGrid />
             </div>
             <div className="flex flex-col w-[90%] md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start md:p-8">
                 <div  className='flex flex-row w-fit justify-center items-center gap-4 md:order-3'>
