a21f378ab45367e99a1443bc4b3396dc204de2ca
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-13 17:44:56 +0800

    Updated Mobile Spacing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,11 +8,11 @@ export default function Hero() {
     const { header, subheader } = HeroSectionHeading;
 
     return(
-        <div className="sectionPadding md:!pt-44 flex flex-col md:flex-row justify-center items-center gap-12 w-full overflow-hidden">
-            <div className="w-full lg:w-[40%] z-10">
+        <div className="sectionPadding !pt-24 md:!pt-32 flex flex-col md:flex-row justify-center items-center gap-12 w-full overflow-hidden">
+            <div className="w-[80%] lg:w-[40%] z-10">
                 <GalleryGrid />
             </div>
-            <div className="flex flex-col w-full md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start md:p-8">
+            <div className="flex flex-col w-[90%] md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start md:p-8">
                 <div  className='flex flex-row w-fit justify-center items-center gap-4 md:order-3'>
                     <Links/>
                 </div>
