df8ae299a02f1676ed5e3a6eff6963d8c3f4f11a
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-13 16:20:28 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,8 +7,10 @@ export default function Footer() {
     return(
         <footer className="flex flex-col w-full py-6 px-16 border-t border-nav-border">
             <div className="flex-col text-center sm:flex-row sm:text-left flexBetween footer_copyright gap-8">
-                <p>{copyright}</p>
-                <SocialMediaLinks/>
+                <div className="sm:order-2">
+                    <SocialMediaLinks/>
+                </div>
+                <p className="sm:order-1">{copyright}</p>
             </div>
         </footer>
     )
