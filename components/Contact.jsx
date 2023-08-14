b3579d80ef08087b0bd42294910184bb5e077670
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-14 17:38:54 +0800

    Updated contact information

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -66,10 +66,10 @@ export default function Contact() {
 
     return( 
         <div id='contact' className='flex flex-row flex-wrap w-full sectionPadding text-center justify-center items-center md:text-left gap-8'>
-          <div className='flex flex-col justify-between lg:flex-row gap-8 w-full lg:w-[55%] h-full bg-white shadow-xl rounded-xl p-8 md:order-2 border border-neutral-300'>
+          <div className='flex flex-col justify-between lg:flex-row gap-8 w-full lg:w-[55%] h-full bg-white shadow-xl rounded-xl p-4 lg:p-8 lg:order-2 border border-neutral-300'>
             <ContactGrid/>
           </div>
-          <div className='flex flex-col gap-4 shadow-xl rounded-xl p-4 w-full md:p-8 lg:w-[40%] bg-neutral-800 text-white m-2 md:mr-0 md:order-1'>
+          <div className='flex flex-col gap-4 shadow-xl rounded-xl p-4 w-full md:p-8 lg:w-[40%] bg-neutral-800 text-white m-2 md:mr-0 lg:order-1'>
               <div className="">
                   <h1 className="sectionOverline !text-white">{overline}</h1>
                   <h1 className="sectionHeader">{header}</h1>
--- constants/SectionContent.tsx
+++ constants/SectionContent.tsx
@@ -171,5 +171,5 @@ export const ProjectSectionContent = [
 export const ContactSectionContent = [
     { key:1, icon: <HiMailOpen size={contactIconSize}/>, title:"Email Address", text:"andreiwork25@gmail.com"},
     { key:2, icon: <GiRotaryPhone size={contactIconSize}/>, title:"Phone Number", text:"*639763272778"},
-    { key:3, icon: <MdAccessTimeFilled size={contactIconSize}/>, title:"Working Hours", text:"(GMT+8) 09:00 - 17:00"},
+    { key:3, icon: <MdAccessTimeFilled size={contactIconSize}/>, title:"Working Hours", text:"(GMT+8) 9:00am-6:00pm"},
 ]
