0b9c46f71d3a8b288ddadbf6dd8abf003fa7a80a
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-16 19:57:42 +0800

    Cleaned up scripts
    +Removed unused scripts
    +Created a folder of yet to be used scripts

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,9 @@
-import { EarthCanvas, StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
 import Technologies from "./Technologies";
 import Projects from "./Projects";
 import Contact from "./Contact";
-import CanvasLoader from "./Loader";
 
 export {
   Navbar,
@@ -14,7 +12,4 @@ export {
   Technologies,
   Projects,
   Contact,
-  CanvasLoader,
-  EarthCanvas, 
-  StarsCanvas, 
 };
--- components/unused/WorkExperience.tsx
+++ components/unused/WorkExperience.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 import { WorkExperienceSectionHeading } from '@/constants/SectionHeading';
 
-export default function WorkExperience() {
+type TimelineProps = {
+  timeline: Array<TimelineCardProps>,
+}
+
+type TimelineCardProps = {
+  position: string,
+  timeframe: string,
+  description: string,
+  contributions: Array<String>,
+}
+
+export default function WorkExperience( { timeline }:TimelineProps ) {
   const { overline, header } = WorkExperienceSectionHeading;
 
   return (
@@ -10,6 +21,9 @@ export default function WorkExperience() {
         <div className="">
             <h1 className="sectionOverline">{overline}</h1>
             <h1 className="sectionHeader">{header}</h1>
+        </div>
+        <div>
+          Card based timeline for ongoing work experience.
         </div>
     </div>
   )
