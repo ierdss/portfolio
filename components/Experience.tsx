1dd49199dc86ff2b303dea6522d80142a81e7da6
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-21 07:35:19 +0800

    Format on save

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,30 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import { ExperienceSectionHeading } from '@/constants';
+import { ExperienceSectionHeading } from "@/constants";
 
 type TimelineProps = {
-  timeline?: Array<TimelineCardProps>,
-}
+  timeline?: Array<TimelineCardProps>;
+};
 
 type TimelineCardProps = {
-  position: string,
-  timeframe: string,
-  description: string,
-  contributions: Array<String>,
-}
+  position: string;
+  timeframe: string;
+  description: string;
+  contributions: Array<String>;
+};
 
-export default function Experience( { timeline }:TimelineProps ) {
+export default function Experience({ timeline }: TimelineProps) {
   const { overline, header } = ExperienceSectionHeading;
 
   return (
-    <div className='w-full sectionPadding'>
-        <div className="">
-            <h1 className="sectionOverline">{overline}</h1>
-            <h1 className="sectionHeader">{header}</h1>
-        </div>
-        <div>
-          Card based timeline for ongoing work experience.
-        </div>
+    <div className="w-full sectionPadding">
+      <div className="">
+        <h1 className="sectionOverline">{overline}</h1>
+        <h1 className="sectionHeader">{header}</h1>
+      </div>
+      <div>Card based timeline for ongoing work experience.</div>
     </div>
-  )
+  );
 }
