3aa7ecb80158fc73155686e784b851a0631ca764
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-20 21:04:47 +0800

    Services section layout done

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,16 +1,37 @@
-export default function Services() {
-    return(
-        <div>
-            <div>
-                <div className='w-full'>
-                    <h1 className="sectionOverline">services</h1>
-                    <h1 className="sectionHeader">what can i provide?</h1>
-                </div>
-                <p className="sectionDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo praesentium adipisci ullam sint placeat doloremque, voluptatibus veniam minima eos. Explicabo!</p>
-            </div>
-            <div className="serviceCards">
+import Image from "next/image";
+import { ServiceCard } from "./cards/ServiceCards";
+import { ServicesSectionHeading } from "@/constants"
 
+export default function Services() {
+    const { overline, header, description } = ServicesSectionHeading
+
+    return(
+        <div className="services sectionPadding">
+            <div className="servicesContent">
+                <div className='w-full'>
+                    <h1 className="sectionOverline">{overline}</h1>
+                    <h1 className="sectionHeader">{header}</h1>
+                </div>
+                <p className="sectionDescription">{description}</p>
             </div>
+            <div className="servicesCards">
+                <div className="servicesCardsRow">
+                    <ServiceCard colorType="serviceCard__red" />
+                    <ServiceCard colorType="serviceCard__black" />
+                </div>
+                <div className="servicesCardsRow">
+                    <ServiceCard colorType="serviceCard__black" />
+                    <ServiceCard colorType="serviceCard__red" /> 
+                </div>
+            </div>
+            <Image
+                src={"/patterns/bg-services.svg"}
+                fill
+                alt="Circles"
+                className="servicesBackground"
+            >
+
+            </Image>
         </div>
     )
 }
