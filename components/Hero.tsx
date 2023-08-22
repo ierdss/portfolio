dc12067fa03777459e671df282bc9123f1068383
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-23 00:50:36 +0800

    Hero section is done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import Image from "next/image";
 import Links from "./minor/Links";
 import { HeroSectionHeading } from "@/constants/SectionHeading";
+import Link from "next/link";
 
 export default function Hero() {
   const { header, body } = HeroSectionHeading;
@@ -76,13 +77,21 @@ export default function Hero() {
         </div>
       </div>
       <div className="flex flex-col w-[90%] md:w-[440px] gap-4 text-center justify-center items-center md:text-left md:items-start">
-        <div className="flex flex-row w-fit justify-center items-center gap-4 md:order-3">
+        <div className="flex flex-col w-fit justify-center items-center gap-4 md:order-3">
           <Links />
         </div>
         <h1 className="font-bold text-3xl md:order-1">{header}</h1>
         <p className="md:order-2 text-sm font-bold text-slate-600 normal-case">
           {body}
         </p>
+        {/* <div className="md:order-4 w-full flex flex-row gap-[10px] justify-between items-center">
+          <button type="button" className="sectionCta__outline">
+            Download CV
+          </button>
+          <Link href="#contact" className="callToAction">
+            Say hello
+          </Link>
+        </div> */}
       </div>
     </div>
   );
