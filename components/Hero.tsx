11e1845d7ca0966fde87c62afe982fdd29e0b695
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-22 22:11:27 +0800

    Hero section almost complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,82 @@
 "use client";
 
+import { useEffect } from "react";
+import Image from "next/image";
 import Links from "./minor/Links";
 import { HeroSectionHeading } from "@/constants/SectionHeading";
-import HeroGrid from "./minor/HeroGrid";
 
 export default function Hero() {
   const { header, subheader } = HeroSectionHeading;
 
+  useEffect(() => {
+    let profileCardEl = document.querySelectorAll<HTMLElement>(".heroCard");
+
+    profileCardEl.forEach((heroCard) => {
+      heroCard.addEventListener("mouseover", function () {
+        profileCardEl.forEach((eachCard) => {
+          eachCard.classList.remove("active");
+        });
+        heroCard.classList.add("active");
+      });
+    });
+  }, []);
+
   return (
-    <div className="sectionPadding md:!pt-32 flex flex-col md:flex-row justify-center items-center gap-12 w-full overflow-hidden mt-[50px]">
-      <div className="w-[80%] lg:w-[40%] z-10">
-        <HeroGrid />
+    <div
+      id="hero"
+      className="hero sectionPadding md:!px-[100px] md:!pt-[100px]"
+    >
+      <div className="heroContainer">
+        <div className="heroCard">
+          <Image
+            src="/placeholder/man-left.jpg"
+            fill
+            alt="man-left"
+            priority
+            quality={100}
+            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, (max-width: 1280px) 100vw, 25vw"
+            className="heroCard__image"
+          />
+          <div className="heroCard__content">
+            <h1 className="sectionHeader">Andrei</h1>
+            <h1 className="sectionOverline !text-white">Designer</h1>
+          </div>
+        </div>
+
+        <div className="heroCard">
+          <Image
+            src="/placeholder/man-center.jpg"
+            fill
+            alt="man-center"
+            priority
+            quality={100}
+            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, (max-width: 1280px) 100vw, 25vw"
+            className="heroCard__image"
+          />
+          <div className="heroCard__content">
+            <h1 className="sectionHeader">Santos</h1>
+            <h1 className="sectionOverline !text-white">Developer</h1>
+          </div>
+        </div>
+
+        <div className="heroCard active">
+          <Image
+            src="/placeholder/man-right.jpg"
+            fill
+            alt="man-right"
+            priority
+            quality={100}
+            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, (max-width: 1280px) 100vw, 25vw"
+            className="heroCard__image"
+          />
+          <div className="heroCard__content">
+            <h1 className="sectionHeader">Sager</h1>
+            <h1 className="sectionOverline !text-white">Innovator</h1>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col w-[90%] md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start md:p-8">
+
+      {/* <div className="flex flex-col w-[90%] md:w-[500px] gap-4 text-center justify-center items-center md:text-left md:items-start">
         <div className="flex flex-row w-fit justify-center items-center gap-4 md:order-3">
           <Links />
         </div>
@@ -20,7 +84,7 @@ export default function Hero() {
         <p className="md:order-2 font-bold text-slate-600 normal-case">
           {subheader}
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
