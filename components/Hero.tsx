460df9478ac08757ff3f7431b42b684a949b9fa6
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-30 06:50:24 +0800

    Update Hero.tsx
    +successively appearing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -60,7 +60,7 @@ export default function Hero() {
           style={{
             transform: isInView ? "translateY(50px)" : "translateY(-200px)",
             opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
           }}
           className="heroCard"
         >
@@ -84,7 +84,7 @@ export default function Hero() {
           style={{
             transform: isInView ? "translateY(0px)" : "translateY(200px)",
             opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
           }}
           className="heroCard"
         >
@@ -108,7 +108,7 @@ export default function Hero() {
         style={{
           transform: isInView ? "translateX(0px)" : "translateX(200px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 2s",
         }}
         className="flex flex-col w-[90%] md:w-[440px] gap-4 text-center justify-center items-center md:text-left md:items-start"
       >
