aed473bafb32bae20bf7d781905ec4f7a662176f
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-30 14:13:05 +0800

    Cleaned up typography
    +All typography is now uniform

--- components/WorkFlow.tsx
+++ components/WorkFlow.tsx
@@ -14,7 +14,7 @@ export default function Workflow() {
         <h1 className="sectionHeading !text-center">{header}</h1>
         <span className="w-[60px] h-[5px] bg-secondary-red mt-2 rounded-md" />
       </div>
-      <ul className="w-full grid grid-cols-1 md:grid-cols-4 gap-[20px]">
+      <ul className="w-full grid grid-cols-1 md:grid-cols-2 lg:px-[100px] gap-[50px]">
         {WorkflowSectionContent.map(({ id, number, title, description }) => (
           <li
             key={id}
@@ -23,8 +23,8 @@ export default function Workflow() {
             <span className="w-[50px] aspect-square absolute -top-[30px] left-[50%] -translate-x-1/2 bg-secondary-red rounded-full text-white font-bold text-2xl flex items-center justify-center shadow-lg !z-20">
               {number}
             </span>
-            <h1 className="font-bold text-xl capitalize">{title}</h1>
-            <p>{description}</p>
+            <h1 className="sectionHeading-2 !text-center">{title}</h1>
+            <p className="sectionDescription !text-center">{description}</p>
             <span className="ease absolute top-0 left-0  h-0 w-0 border-t-2 border-secondary-red transition-all duration-500 group-hover:w-1/2" />
             <span className="ease absolute top-0 right-0 h-0 w-0 border-b-2 border-secondary-red transition-all duration-500 group-hover:w-1/2 rounded-lg" />
             <span className="ease absolute bottom-0 left-0  h-0 w-0 border-t-2 border-secondary-red transition-all duration-1000 group-hover:w-1/2" />
