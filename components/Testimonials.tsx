7e5df36bf8c79ae4e4fd84cbf283b5bfe258e6e5
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-08-30 11:39:20 +0800

    Updated Testimonials.tsx

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,7 +3,6 @@ import {
   TestimonialsSectionContent,
 } from "@/constants";
 import Image from "next/image";
-import { BiSolidQuoteLeft } from "react-icons/bi";
 
 export default function Testimonials() {
   const { overline, header } = TestimonialsSectionHeading;
@@ -15,33 +14,30 @@ export default function Testimonials() {
         <h1 className="sectionHeader">{header}</h1>
         <span className="w-[60px] h-[5px] bg-secondary-red mt-2 rounded-md" />
       </div>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[75px] gap-x-[25px]">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[20px] gap-x-[25px]">
         {TestimonialsSectionContent.map(
           ({ avatar, name, occupation, testimonial }, index) => (
             <li
               key={index}
-              className="relative flex items-center flex-col justify-around w-full rounded-md p-8 gap-3 shadow-lg border border-neutral-300 group first:text-white first:bg-secondary-red bg-neutral-50 pt-[50px] hover:-translate-y-4 transition-all duration-500 ease-in-out"
+              className="relative flex items-center flex-col w-full rounded-md p-8 gap-3 shadow-lg border border-neutral-300 group first:text-white first:bg-secondary-red bg-neutral-50  hover:-translate-y-4 transition-all duration-500 ease-in-out"
             >
-              <Image
-                src={avatar}
-                width={90}
-                height={90}
-                alt="Testimonial Avatar"
-                className="absolute -top-[50px] left-[50%] -translate-x-1/2 bg-black rounded-full overflow-hidden aspect-square"
-              />
-              <div className="w-full flexCenter flex-row !justify-between">
+              <p className="font-medium text-xs text-left">{testimonial}</p>
+
+              <div className="w-full flexCenter flex-row !justify-start gap-2">
+                <Image
+                  src={avatar}
+                  width={50}
+                  height={50}
+                  alt="Testimonial Avatar"
+                  className=" bg-black rounded-full overflow-hidden aspect-square"
+                />
                 <div className="text-left">
-                  <h1 className="font-bold text-lg">{name}</h1>
-                  <h1 className="font-semibold text-xs text-secondary-red group-first:text-white uppercase">
+                  <h1 className="font-bold text-sm">{name}</h1>
+                  <h1 className="font-semibold text-xs text-secondary-red group-first:text-white ">
                     {occupation}
                   </h1>
                 </div>
-                <BiSolidQuoteLeft
-                  size={40}
-                  className="text-secondary-red group-first:text-white"
-                />
               </div>
-              <p className="font-medium text-xs text-left">{testimonial}</p>
             </li>
           )
         )}
