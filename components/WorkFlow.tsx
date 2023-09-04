db892ef0ba04d7906af3c0c4a7f88fff7e54bfa6
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-09-04 19:24:03 +0800

    Updated content of workflow

--- components/WorkFlow.tsx
+++ components/WorkFlow.tsx
@@ -11,17 +11,19 @@ export default function Workflow() {
         <h1 className="sectionHeading !text-center">My Thought Process</h1>
         <span className="w-[60px] h-[5px] bg-secondary-red mt-2 rounded-md" />
       </div>
-      <ul className="w-full grid grid-cols-1 md:grid-cols-4 gap-[30px]">
-        {WorkflowSectionContent.map(({ id, number, title, description }) => (
+      <ul className="w-full grid grid-cols-1 md:grid-cols-4 gap-[30px] md:gap-[25px]">
+        {WorkflowSectionContent.map(({ number, title, description }, index) => (
           <li
-            key={id}
+            key={index}
             className="w-full aspect-video relative border border-neutral-100 rounded-md bg-white hover:shadow-xl hover:-translate-y-3 transition-all duration-500 px-4 py-10 text-center group inline-block z-10"
           >
             <span className="w-[50px] aspect-square absolute -top-[30px] left-[50%] -translate-x-1/2 bg-secondary-red rounded-full text-white font-bold text-2xl flex items-center justify-center shadow-lg !z-20">
               {number}
             </span>
             <h1 className="sectionHeading-2 !text-center">{title}</h1>
-            <p className="sectionDescription !text-center">{description}</p>
+            <p className="sectionDescription !text-justify mt-2">
+              {description}
+            </p>
             <span className="ease absolute top-0 left-0  h-0 w-0 border-t-2 border-secondary-red transition-all duration-500 group-hover:w-1/2" />
             <span className="ease absolute top-0 right-0 h-0 w-0 border-b-2 border-secondary-red transition-all duration-500 group-hover:w-1/2 rounded-lg" />
             <span className="ease absolute bottom-0 left-0  h-0 w-0 border-t-2 border-secondary-red transition-all duration-1000 group-hover:w-1/2" />
