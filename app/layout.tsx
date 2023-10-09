694cb1d0be1761b080e7e4a5693e80145b7c04c4
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-10-09 08:50:29 +0800

    Navbar
    +Fixed bugging navbar
    ?Found another bug that does not render mobile nav properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Andrei Sager Portfolio',
-  description: 'A next.js portfolio',
-}
+  title: "Andrei Sager Portfolio",
+  description: "A next.js portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='flex justify-center'>
-          <div className='flex flex-wrap w-full max-w-screen-xl'>
-            <Navbar/>
-            <main className='w-full'>
-              {children}
-            </main>
-            <Footer/>
+        <div className="flex justify-center w-full">
+          <div className="flex flex-col w-screen max-w-screen-xl">
+            <Navbar />
+            <main className="w-screen">{children}</main>
+            <Footer />
           </div>
         </div>
       </body>
     </html>
-  )
+  );
 }
