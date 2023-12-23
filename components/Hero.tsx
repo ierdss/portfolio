6366bd2607a7b1ed468610098d2f1b79a7198c4e
LordBrim <andreiwork25@gmail.com>
2023-12-23 08:52:47 +0800

    Updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import Links from "./minor/Links";
+
 import { motion, useInView } from "framer-motion";
+import Link from "next/link";
 
 export default function Hero() {
   const ref = useRef(null);
@@ -36,85 +37,15 @@ export default function Hero() {
       id="hero"
       className="hero sectionPadding md:!px-[100px] md:!pt-[100px] relative overflow-hidden"
     >
-      <div className="heroContainer z-10">
-        <motion.div
-          ref={ref}
-          style={{
-            transform: isInView ? "translateY(0px)" : "translateY(200px)",
-            opacity: isInView ? 1 : 0,
-            transition: isDone
-              ? ""
-              : "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0s",
-          }}
-          className="heroCard active"
-        >
-          <Image
-            src="/placeholder/man-left.jpg"
-            fill
-            alt="man-left"
-            priority
-            quality={100}
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, (max-width: 1280px) 100vw, 25vw"
-            className="heroCard__image"
-          />
-          <div className="heroCard__content">
-            <h1 className="sectionHeading">Andrei</h1>
-            <p className="sectionOverline !text-white">Developer</p>
-          </div>
-        </motion.div>
-
-        <motion.div
-          ref={ref}
-          style={{
-            transform: isInView ? "translateY(50px)" : "translateY(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: isDone
-              ? ""
-              : "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-          className="heroCard"
-        >
-          <Image
-            src="/placeholder/man-center.jpg"
-            fill
-            alt="man-center"
-            priority
-            quality={100}
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, (max-width: 1280px) 100vw, 25vw"
-            className="heroCard__image"
-          />
-          <div className="heroCard__content">
-            <h1 className="sectionHeading">Santos</h1>
-            <p className="sectionOverline !text-white">Designer</p>
-          </div>
-        </motion.div>
-
-        <motion.div
-          ref={ref}
-          style={{
-            transform: isInView ? "translateY(0px)" : "translateY(200px)",
-            opacity: isInView ? 1 : 0,
-            transition: isDone
-              ? ""
-              : "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
-          }}
-          className="heroCard"
-        >
-          <Image
-            src="/placeholder/man-right.jpg"
-            fill
-            alt="man-right"
-            priority
-            quality={100}
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 75vw, (max-width: 1280px) 100vw, 25vw"
-            className="heroCard__image"
-          />
-          <div className="heroCard__content">
-            <h1 className="sectionHeading">Sager</h1>
-            <p className="sectionOverline !text-white">Solver</p>
-          </div>
-        </motion.div>
+      <div className="w-[30%] aspect-square flex items-center justify-center rounded-full overflow-hidden">
+        <Image
+          src="/hero.jpg"
+          width={1000}
+          height={1000}
+          alt="A picture of Andrei Sager"
+        />
       </div>
+
       <motion.div
         ref={ref}
         style={{
@@ -124,36 +55,25 @@ export default function Hero() {
         }}
         className="flex flex-col w-[90%] md:w-[300px] gap-4 text-center justify-center items-center lg:items-start shrink-0"
       >
-        <div className="flex flex-col w-fit justify-center items-center gap-4 md:order-3">
-          <Links />
-        </div>
-        <h1 className="sectionHeading md:order-1 !text-center lg:!text-left">
+        <h1>Hi My Name is Andrei, I am a</h1>
+        <h1 className="sectionHeading !text-center lg:!text-left">
           Software Developer
         </h1>
-        <p className="md:order-2 sectionLeading !text-center lg:!text-left">
-          Embracing the fusion of artistry and code, I shape digital experiences
-          as a full-stack software developer. Building experiences with a twist.
+        <p className="sectionLeading !text-center lg:!text-left">
+          The value I can bring Embracing the fusion of artistry and code, I
+          shape digital experiences as a full-stack software developer. Building
+          experiences with a twist.
         </p>
+        <div className="flex flex-col w-fit justify-center items-center gap-4 md:order-3"></div>
+        <div className="flex w-full">
+          <Link href="#contact" className="navbarCta !block">
+            Download CV
+          </Link>
+          <Link href="#contact" className="navbarCta !block">
+            Contact
+          </Link>
+        </div>
       </motion.div>
-      {/* <div className="servicesBackground aspect-square !opacity-30 top-[100px] left-0 -z-0">
-        <Image
-          typeof="background"
-          src={"/patterns/ring-top.svg"}
-          width={1000}
-          height={1000}
-          alt="This is impossible! An svg did not load?"
-          className="md:hidden object-none overflow-auto aspect-square"
-        />
-      </div>
-      <div className="servicesBackground !opacity-20 right-0 -z-0 hidden md:flex mix-blend-darken">
-        <Image
-          typeof="background"
-          src={"/patterns/ring-splitter.svg"}
-          width={1000}
-          height={1000}
-          alt="This is impossible! An svg did not load?"
-        />
-      </div> */}
     </div>
   );
 }
