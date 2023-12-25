85bea2eee77b991c3238bcc8204c81967253aa10
LordBrim <andreiwork25@gmail.com>
2023-12-26 05:33:17 +0800

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,23 +1,54 @@
+import { useRef } from "react";
+
 import Image from "next/image";
 import { ServiceCard } from "./cards/ServiceCards";
 import { ServicesSectionContent as card } from "@/constants";
 
+import { motion, useInView } from "framer-motion";
+
 export default function Services() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <div className="w-full  bg-rose-50 flex items-center justify-center">
       <div id="services" className="services sectionPadding">
         <div className="servicesContent">
           <div className="w-full  md:w-full">
-            <h1 className="sectionOverline sm:text-center md:text-left">
+            <motion.h1
+              className="sectionOverline sm:text-center md:text-left"
+              ref={ref}
+              style={{
+                transform: isInView ? "translateY(0px)" : "translateY(200px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+              }}
+            >
               Services
-            </h1>
-            <h1 className="sectionHeading sm:text-center md:text-left">
-              What I Offer
-            </h1>
+            </motion.h1>
+            <motion.h1
+              className="sectionHeading sm:text-center md:text-left"
+              ref={ref}
+              style={{
+                transform: isInView ? "translateY(0px)" : "translateY(200px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
+              }}
+            >
+              What I Can Offer
+            </motion.h1>
           </div>
-          <p className="sectionLeading sm:text-center md:text-left">
+          <motion.p
+            className="sectionLeading sm:text-center md:text-left"
+            ref={ref}
+            style={{
+              transform: isInView ? "translateY(0px)" : "translateY(200px)",
+              opacity: isInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
+            }}
+          >
             With my services, I can make your product ideas come to life.
-          </p>
+          </motion.p>
         </div>
         <div className="servicesCards relative">
           <Image
