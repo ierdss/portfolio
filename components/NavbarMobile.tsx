26ddcc1ef6e4c8e7ee1ac3a9af0f4c1a278e3c99
LordBrim <andreiwork25@gmail.com>
2023-12-27 10:08:33 +0800

    Cleaned up css using BEM

---
+++ components/NavbarMobile.tsx
@@ -0,0 +1,64 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
+import { motion, useCycle } from "framer-motion";
+
+import { NavLinks } from "@/constants/NavigationLinks";
+import Links from "./minor/Links";
+import Link from "next/link";
+
+export default function NavbarMobile() {
+  const sizeIcon = 30;
+  const [isOpen, toggleIsOpen] = useCycle(false, true);
+
+  return (
+    <motion.div
+      className={isOpen ? "" : ""}
+      animate={isOpen ? "open" : "closed"}
+      variants={variants}
+    >
+      <div onClick={() => useCycle()} className="navbar-mobile__toggle ">
+        <GiHamburgerMenu size={sizeIcon} />
+      </div>
+      <div onClick={() => useCycle()} className="navbar__mobile-top">
+        <h1 className="navbarLogo bg-clip">ANDREI</h1>
+        <RxCross2 size={sizeIcon} />
+      </div>
+      <ul id="navbar-links" className="navbarLinks">
+        {NavLinks.map(({ href, text }, index) => (
+          <li key={index} className="navbarListItem">
+            <Link onClick={() => useCycle()} href={href} className="navbarLink">
+              {text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Links />
+    </motion.div>
+  );
+}
+
+// Framer Motion Variants
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+};
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
