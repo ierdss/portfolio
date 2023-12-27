71e0d407c2d3b54e40b5a15bb503a4291be5d2b7
LordBrim <andreiwork25@gmail.com>
2023-12-27 08:25:43 +0800

    Removed unnecessary constants

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,7 @@
-import SocialMediaLinks from "./minor/Links";
-import { FooterSectionHeading } from "@/constants/SectionHeading";
-
 export default function Footer() {
-  const { copyright } = FooterSectionHeading;
-
   return (
     <footer className="flex flex-col w-full py-6 px-16 border-t border-neutral-300  text-center">
-      {copyright}
+      @ 2023 Andrei Sager. All rights reserved
     </footer>
   );
 }
