8febe4b79a3461059d2b1b40ac473306cac8fcd6
LordBrim <andreiwork25@gmail.com>
2024-01-08 13:06:43 +0800

    Added columns

--- components/projects/OtherProjects.tsx
+++ components/projects/OtherProjects.tsx
@@ -1,7 +1,7 @@
 export default function OtherProjects() {
   return (
     <div>
-      <div className="">
+      <div className="grid-cols-1 md:grid-cols-3">
         <h1 className="sectionOverline"></h1>
         <h1 className="sectionHeader"></h1>
       </div>
