9e50d67bfe6fb3517be879eb3e2d55b2c2e5d939
LordBrim <andreiwork25@gmail.com>
2024-01-08 11:31:19 +0800

    Added database

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,148 +1,68 @@
-import { Projects as ProjectList } from "@/constants/projectsdatabase";
-import Image from "next/image";
-import Link from "next/link";
-import { BsGithub } from "react-icons/bs";
-import { IoGlobe } from "react-icons/io5";
-import { HiOutlineExternalLink } from "react-icons/hi";
-
 import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
 
-const sizeIcon = 30;
+import { PrismaClient } from "@prisma/client";
+import FeaturedProjects from "./FeaturedProjects";
 
-type TagsProps = {
-  tags: Array<TagProps>;
-};
+export default async function Projects() {
+  const prisma = new PrismaClient();
 
-type TagProps = {
-  id: number;
-  tag: string;
-};
-
-function ProjectTags({ tags }: TagsProps) {
-  return (
-    <ul className="projectTags">
-      {tags.map(({ id, tag }) => (
-        <li key={id} className="projectTag">
-          {tag}
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-type ProjectLinksProps = {
-  githubUrl: string;
-  livedemoUrl: string;
-};
-
-function ProjectLinks({ githubUrl, livedemoUrl }: ProjectLinksProps) {
-  return (
-    <div className="projectLinks">
-      <a
-        href={githubUrl}
-        target="_blank"
-        className="projectLink sectionLeading"
-      >
-        <BsGithub size={sizeIcon} />
-        Source Code
-      </a>
-      <a
-        href={livedemoUrl}
-        target="_blank"
-        className="projectLink sectionLeading"
-      >
-        <IoGlobe size={sizeIcon} />
-        Live Demo
-      </a>
-    </div>
-  );
-}
-
-function FeaturedProjects() {
-  return (
-    <div className="projectsList">
-      {ProjectList.map(
-        ({
-          id,
-          thumbnailDesktop,
-          thumbnailMobile,
-          desktopAlt,
-          mobileAlt,
-          title,
-          subtitle,
-          description,
-          tags,
-          githubUrl,
-          livedemoUrl,
-        }) => (
-          <div key={id} className="projectItem">
-            <div className="projectItem__right">
-              <Link href={livedemoUrl} target="_blank">
-                <div className="projectThumbnails">
-                  <div className="projectThumbnailDesktop">
-                    <Image
-                      src={thumbnailDesktop}
-                      width={950}
-                      height={700}
-                      alt={desktopAlt}
-                      loading="lazy"
-                      quality={100}
-                      sizes="(min-width: 768px) 75vw, (min-width: 1280px) 100vw, 25vw"
-                      className="rounded-2xl object-cover object-top"
-                    />
-                  </div>
-                  <div className="projectThumbnailMobile">
-                    <div>
-                      <Image
-                        src={thumbnailMobile}
-                        width={200}
-                        height={50}
-                        alt={mobileAlt}
-                        loading="lazy"
-                        quality={100}
-                        className="object-cover object-top"
-                      />
-                    </div>
-                  </div>
-                  <HiOutlineExternalLink
-                    size={30}
-                    className="projectThumbnail__external-link"
-                  />
-                </div>
-              </Link>
-              <div className="projectTags__container">
-                <ProjectTags tags={tags} />
-              </div>
-            </div>
-            <div className="projectDetails">
-              <div>
-                <h1 className="sectionHeading-2 !text-center lg:!text-left">
-                  {title}
-                </h1>
-                <h1 className="sectionOverline-2 !text-center lg:!text-left">
-                  {subtitle}
-                </h1>
-              </div>
-              {description}
-              <ProjectLinks githubUrl={githubUrl} livedemoUrl={livedemoUrl} />
-            </div>
-          </div>
-        )
-      )}
-    </div>
-  );
-}
-
-function OtherProjects() {
-  return <div>Other projects, snippets, and contributions</div>;
-}
-
-export default function Projects() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
+  // const project1 = await prisma.project.create({
+  //   data: { title: "Lorebuilder" },
+  // });
+
+  // const project2 = await prisma.project.create({
+  //   data: {
+  //     title: "Roadway Delivery",
+  //   },
+  // });
+
+  // const tag = await prisma.tag.create({
+  //   data: {
+  //     name: "HTML",
+  //     type: "Markup Language",
+  //   },
+  // });
+
+  // const project = await prisma.project.create({
+  //   data: {
+  //     title: "Marked A.I.",
+  //     tags: { connect: { id: "659b60ba7354a795e7d4040d" } },
+  //   },
+  // });
+
+  const projects = await prisma.project.findMany({
+    where: { title: "Lorebuilder" },
+  });
+
+  // const projects = await prisma.project.findMany({
+  //   include: {
+  //     tags: true,
+  //   },
+  // });
+
+  // const tags = await prisma.tag.findMany({
+  //   where: { name: "HTML" },
+  //   include: {
+  //     projects: true,
+  //   },
+  // });
+
   return (
+    // <div>
+    //   <div>Prisma</div>
+    //   {projects.map(({ title, subtitle, description, isFeatured }) => (
+    //     <div>
+    //       <h1>{title}</h1>
+    //       <h1>{subtitle}</h1>
+    //       <p>{description}</p>
+    //       <p>{isFeatured}</p>
+    //     </div>
+    //   ))}
+    // </div>
     <div
       id="projects"
       className="w-full flex flex-col justify-center items-center md:text-left"
@@ -175,10 +95,10 @@ export default function Projects() {
 
         <FeaturedProjects />
         {/* <div className="">
-              <h1 className="sectionOverline">{overline}</h1>
-              <h1 className="sectionHeader">{header}</h1>
-          </div>
-          <OtherProjects/> */}
+                <h1 className="sectionOverline">{overline}</h1>
+                <h1 className="sectionHeader">{header}</h1>
+            </div>
+            <OtherProjects/> */}
       </div>
     </div>
   );
