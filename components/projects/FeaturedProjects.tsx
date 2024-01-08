5e249c22bf4d2d6371596af30307b1eae452598f
LordBrim <andreiwork25@gmail.com>
2024-01-08 13:06:43 +0800

    Added columns

--- components/projects/FeaturedProjects.tsx
+++ components/projects/FeaturedProjects.tsx
@@ -1,7 +1,7 @@
 export default function FeaturedProjects() {
   return (
     <div>
-      <div className="">
+      <div className="grid-cols-1 md:grid-cols-2">
         <h1 className="sectionOverline"></h1>
         <h1 className="sectionHeader"></h1>
       </div>
