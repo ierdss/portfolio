8289fd4fd672a770d8d4fed688d65c2b1ecdce5e
LordBrim <andreiwork25@gmail.com>
2024-01-08 11:31:19 +0800

    Added database

---
+++ components/projects/FeaturedProjectsCard.tsx
@@ -0,0 +1,137 @@
+import { Projects as ProjectList } from "@/constants/projectsdatabase";
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import { IoGlobe } from "react-icons/io5";
+import { HiOutlineExternalLink } from "react-icons/hi";
+
+import { motion } from "framer-motion";
+
+const sizeIcon = 30;
+
+type TagsProps = {
+  tags: Array<TagProps>;
+};
+
+type TagProps = {
+  id: number;
+  tag: string;
+};
+
+function ProjectTags({ tags }: TagsProps) {
+  return (
+    <ul className="projectTags">
+      {tags.map(({ id, tag }) => (
+        <li key={id} className="projectTag">
+          {tag}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+type ProjectLinksProps = {
+  githubUrl: string;
+  livedemoUrl: string;
+};
+
+export default function ProjectLinks({
+  githubUrl,
+  livedemoUrl,
+}: ProjectLinksProps) {
+  return (
+    <div className="projectLinks">
+      <a
+        href={githubUrl}
+        target="_blank"
+        className="projectLink sectionLeading"
+      >
+        <BsGithub size={sizeIcon} />
+        Source Code
+      </a>
+      <a
+        href={livedemoUrl}
+        target="_blank"
+        className="projectLink sectionLeading"
+      >
+        <IoGlobe size={sizeIcon} />
+        Live Demo
+      </a>
+    </div>
+  );
+}
+
+function FeaturedProjects() {
+  return (
+    <div className="projectsList">
+      {ProjectList.map(
+        ({
+          id,
+          thumbnailDesktop,
+          thumbnailMobile,
+          desktopAlt,
+          mobileAlt,
+          title,
+          subtitle,
+          description,
+          tags,
+          githubUrl,
+          livedemoUrl,
+        }) => (
+          <div key={id} className="projectItem">
+            <div className="projectItem__right">
+              <Link href={livedemoUrl} target="_blank">
+                <div className="projectThumbnails">
+                  <div className="projectThumbnailDesktop">
+                    <Image
+                      src={thumbnailDesktop}
+                      width={950}
+                      height={700}
+                      alt={desktopAlt}
+                      loading="lazy"
+                      quality={100}
+                      sizes="(min-width: 768px) 75vw, (min-width: 1280px) 100vw, 25vw"
+                      className="rounded-2xl object-cover object-top"
+                    />
+                  </div>
+                  <div className="projectThumbnailMobile">
+                    <div>
+                      <Image
+                        src={thumbnailMobile}
+                        width={200}
+                        height={50}
+                        alt={mobileAlt}
+                        loading="lazy"
+                        quality={100}
+                        className="object-cover object-top"
+                      />
+                    </div>
+                  </div>
+                  <HiOutlineExternalLink
+                    size={30}
+                    className="projectThumbnail__external-link"
+                  />
+                </div>
+              </Link>
+              <div className="projectTags__container">
+                <ProjectTags tags={tags} />
+              </div>
+            </div>
+            <div className="projectDetails">
+              <div>
+                <h1 className="sectionHeading-2 !text-center lg:!text-left">
+                  {title}
+                </h1>
+                <h1 className="sectionOverline-2 !text-center lg:!text-left">
+                  {subtitle}
+                </h1>
+              </div>
+              {description}
+              <ProjectLinks githubUrl={githubUrl} livedemoUrl={livedemoUrl} />
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+}
---
+++ components/projects/OtherProjectsCard.tsx
@@ -0,0 +1,3 @@
+export default function OtherProjects() {
+  return <div>Other projects, snippets, and contributions</div>;
+}
