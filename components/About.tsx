2076d896f132d8f4f36521269f464592b83bedf6
Andrei <andreiwork25@gmail.com>
2024-01-16 18:15:46 +0800

    Cleaned up about

--- components/About.tsx
+++ components/About.tsx
@@ -35,69 +35,43 @@ export default function About() {
   return (
     <div
       id="about"
-      className="sectionPadding w-full text-center flex justify-center items-center md:text-left flex-col md:flex-row md:min-h-[800px] relative overflow-hidden  max-w-screen-xl"
+      className="about flex flex-row w-full justify-center items-center gap-[50px] md:gap-[100px]"
     >
-      <div className="flex flex-row flex-wrap w-full justify-center items-center gap-[50px] md:gap-[100px]">
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "translateX(0px)" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
-          }}
-          className="hover:-translate-y-2 group transition-all duration-500"
-        >
-          <div className="portraitCard">
-            <div className="portraitCard__back">Back</div>
-            <div className="portraitCard__front">
-              <Image
-                src="/MeRed.png"
-                width={300}
-                height={300}
-                alt="Me in red"
-                className=""
-              />
-            </div>
-          </div>
+      <div className="about-details">
+        <div className="max-w-[250px]">
+          <h1 className="sectionOverline">About</h1>
+          <h1 className="sectionHeading">Get To Know Me</h1>
         </div>
+        <p className="sectionLeading">
+          Sample muna. I'm a natural problem solver, eversince I was a kid I
+          enjoyed building contraptions with lego, solving puzzles, and
+          tinkering my curiosity away. This not only honed my interest in
+          building things but it also polished my persistence and what led me to
+          pursue this career.
+        </p>
+        <p className="sectionLeading">
+          Sample muna. I'm a natural problem solver, eversince I was a kid I
+          enjoyed building contraptions with lego, solving puzzles, and
+          tinkering my curiosity away. This not only honed my interest in
+          building things but it also polished my persistence and what led me to
+          pursue this career.
+        </p>
+      </div>
 
-        <motion.div
-          ref={ref}
-          style={{
-            transform: isInView ? "translateX(0px)" : "translateX(200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
-          }}
-          className="flex flex-col gap-8 w-[90%] lg:w-[45%] "
-        >
-          <div className="flex flex-col gap-4 w-full ">
-            <div className="w-full">
-              <h1 className="sectionOverline">About</h1>
-              <h1 className="sectionHeading">Get To Know Me :)</h1>
-            </div>
-            <p className="sectionLeading">
-              Sample muna. I'm a natural problem solver, eversince I was a kid I
-              enjoyed building contraptions with lego, solving puzzles, and
-              tinkering my curiosity away. This not only honed my interest in
-              building things but it also polished my persistence and what led
-              me to pursue this career.
-            </p>
-            <p className="sectionLeading">
-              Sample muna. I'm a natural problem solver, eversince I was a kid I
-              enjoyed building contraptions with lego, solving puzzles, and
-              tinkering my curiosity away. This not only honed my interest in
-              building things but it also polished my persistence and what led
-              me to pursue this career.
-            </p>
-          </div>
+      <Image
+        src="/MeRed.png"
+        width={300}
+        height={300}
+        alt="Me in red"
+        className="about-portrait"
+      />
 
-          {/* <div className="flex flex-row">
+      {/* <div className="flex flex-row">
             <h1>Projects Completed</h1>
             <h1>Freelance Commisions</h1>
             <h1>Years of Experience</h1>
           </div> */}
-        </motion.div>
-      </div>
+      {/* </motion.div> */}
     </div>
   );
 }
