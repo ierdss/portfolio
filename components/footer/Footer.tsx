fa20014c08b34ee64274af4b0889938f627ce122
Andrei <andreiwork25@gmail.com>
2024-01-18 18:15:47 +0800

    refactored footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="footer">@ 2023 Andrei Sager. All rights reserved</footer>
+    <footer className="footer">
+      <h1 className="footer__title">
+        @ 2023 Andrei Sager. All rights reserved
+      </h1>
+    </footer>
   );
 }
