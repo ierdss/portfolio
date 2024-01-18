dff3f624c29bc6ea14dc42396f775c63bd06c8bb
Andrei <andreiwork25@gmail.com>
2024-01-18 17:44:12 +0800

    Update technologiesdatabase.tsx

--- constants/technologiesdatabase.tsx
+++ constants/technologiesdatabase.tsx
@@ -9,42 +9,42 @@ export const Technologies = [
   {
     icon: <FaHtml5 size={size} color={`#f06529`} />,
     name: "HTML5",
-    link: "",
+    link: "https://www.w3schools.com/html/default.asp",
   },
   {
     icon: <FaCss3Alt size={size} color={`#2965f1`} />,
     name: "CSS3",
-    link: "",
+    link: "https://www.w3schools.com/css/default.asp",
   },
   {
     icon: <SiTailwindcss size={size} color={`#67e8f9`} />,
     name: "Tailwind",
-    link: "",
+    link: "https://tailwindcss.com/docs/installation",
   },
   {
     icon: <FaSass size={size} color={`#cc6699`} />,
     name: "Sass",
-    link: "",
+    link: "https://sass-lang.com/documentation/",
   },
   {
     icon: <IoLogoJavascript size={size} color={`#ecd120`} />,
     name: "Javascript",
-    link: "",
+    link: "https://www.w3schools.com/js/default.asp",
   },
   {
     icon: <BiLogoTypescript size={size} color={`#007acc`} />,
     name: "Typescript",
-    link: "",
+    link: "https://www.typescriptlang.org/docs/",
   },
   {
     icon: <FaReact size={size} color={`#61dbfb`} />,
     name: "React",
-    link: "",
+    link: "https://react.dev/reference/react",
   },
   {
     icon: <SiNextdotjs size={size} color={`#171515`} />,
     name: "Next",
-    link: "",
+    link: "https://nextjs.org/docs",
   },
   {
     icon: null,
