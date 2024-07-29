ccffdf613cd236ba27f64774a253d6f9fe4e7a3e
Andrei <andreiwork25@gmail.com>
2024-07-29 16:55:37 +0800

    Removed experience in the meantime

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <Services />
         <About />
         {/* <Workflow /> */}
-        <Experience />
+        {/* <Experience /> */}
         <Technologies />
         <Projects />
         {/* <Testimonials /> */}
