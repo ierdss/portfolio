488e57d6bd73ac409ca388fd37161416dd2535a7
Andrei <andreiwork25@gmail.com>
2024-09-04 13:06:45 +0800

    Refactored footer CSS into Tailwind CSS

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="footer">
-      <h1 className="footer__title">
+    <footer className="flex">
+      <h1 className="flex flex-col w-full py-6 px-16 text-center">
         @ 2023 Andrei Sager. All rights reserved
       </h1>
     </footer>
