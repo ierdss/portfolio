4711518fe023ebd754b55983be728e673c222a72
Andrei <andreiwork25@gmail.com>
2024-09-04 23:31:48 +0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
       id="hero"
       className="flex w-full flex-col items-center justify-center bg-blackberry"
     >
-      <div className="relative flex h-[100vh] max-h-[800px] w-full max-w-[1980px] flex-col items-center justify-center gap-[40px] overflow-hidden  px-[8px] py-[50px] shadow-lg md:flex-row md:gap-[50px] md:px-[150px] md:py-[100px]">
+      <div className="relative flex h-[100vh] max-h-[800px] w-full max-w-[1980px] flex-col items-center justify-center gap-[40px] overflow-hidden  px-[8px] py-[50px] md:flex-row md:gap-[50px] md:px-[150px] md:py-[100px]">
         <div className="hero__portrait">
           <Image
             src="/hero.png"
