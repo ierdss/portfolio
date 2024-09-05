a894770e71dada14565a51dfeff841cddde66076
Andrei <andreiwork25@gmail.com>
2024-09-05 18:13:23 +0800

    Update Testimonials.tsx

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -50,7 +50,7 @@ function TestimonialsRow({ row }: ITestimonialsRow) {
       {row.map(({ id, avatar, name, location, testimonial }) => (
         <li
           key={id}
-          className="group relative flex w-full select-none flex-col items-center justify-start gap-[24px] rounded-[20px] bg-glass p-5 text-content transition-all duration-500 ease-in-out hover:-translate-y-4 md:h-fit md:min-h-[250px] md:w-[400px] md:p-10"
+          className="group relative flex w-full select-none flex-col items-center justify-start gap-[24px] rounded-[20px] bg-glass p-5 text-content transition-all duration-500 ease-in-out hover:-translate-y-1 md:h-fit md:min-h-[250px] md:w-[400px] md:p-10 md:hover:-translate-y-4"
         >
           <div className="flex w-full flex-row items-center justify-between">
             <div className="flex w-full flex-row items-center justify-start gap-[10px]">
