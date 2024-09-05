df28e14ad0a07b47482054a11d97035b3de2c090
Andrei <andreiwork25@gmail.com>
2024-09-05 21:59:00 +0800

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -35,51 +35,59 @@ export default function About() {
   return (
     <div
       id="about"
-      className="flex-rowitems-center bg-background-1  relative flex w-full justify-center gap-[50px] overflow-hidden text-center md:h-[100vh] md:max-h-[800px] md:flex-row md:gap-[100px] md:text-left"
+      className="flex-rowitems-center relative flex w-full flex-col items-center justify-center gap-[50px] overflow-hidden bg-background-1 text-center md:flex-row md:gap-[100px] md:text-left"
     >
-      <div className="about-portrait">
-        <Image
-          src="/1styear.jpg"
-          width={800}
-          height={800}
-          alt="Me in red"
-          className="about-portrait__image"
-        />
-      </div>
-
-      <div className="about-details">
-        <div className="max-w-[250px]">
-          <h1 className="sectionOverline">About</h1>
-          <h1 className="sectionHeading">Get To Know Me</h1>
+      <div className="relative flex w-full max-w-[1980px] flex-col items-center justify-center gap-[35px] px-[8px] py-[50px] md:flex-row md:gap-[70px] md:px-[150px] md:py-[200px]">
+        <div className="flex w-[550px] items-center justify-center overflow-hidden rounded-xl bg-secondary-red">
+          <Image
+            src="/1styear.jpg"
+            width={800}
+            height={800}
+            alt="Me in red"
+            className="w-[550px]"
+          />
         </div>
-        <p className="about-description">
-          Hey there! I'm Andrei Sager, a software developer residing in the
-          Philippines. I am a student of STI College Sta. Mesa who is currently
-          finishing his Bachelors of Science in Computer Science (BSCS) Degree.
-        </p>
-        <p className="about-description">
-          During my free time I am a self-taught learner exploring deeper into
-          topics outside the scope of the college curriculum diving into
-          practice and creating my personal projects. I'm big on keeping my code
-          clean with methodologies like BEM and am always up for learning
-          something new.
-        </p>
-        <p className="about-description">
-          People often describe me as a motivated and organized person. I like
-          setting goals for projects and making sure I hit them. I believe in
-          bringing out the best results for the companies I work with, all while
-          keeping things dynamic and enjoyable.
-        </p>
-        <p className="about-description">
-          If you ever want to chat about coding, challenges, or just share some
-          cool ideas, I'm all ears! Let's keep making things better together.
-        </p>
-        {/* <div className="flex flex-row">
+
+        <div className="flex w-full max-w-[925px] flex-col gap-[70px]">
+          <h1 className="z-20 w-full text-left text-[40px] font-bold capitalize text-gray  md:text-[96px]">
+            Who Am I?
+          </h1>
+
+          <div className="font-regular flex flex-col gap-4 text-left text-2xl normal-case text-content first:text-white">
+            <p>
+              Hey there! I'm <span className="text-redberry">Andrei Sager</span>
+              , a software developer residing in the Philippines. I am a student
+              of STI College Sta. Mesa who is currently finishing his Bachelors
+              of Science in Computer Science (BSCS) Degree.
+              <br />
+              <br />
+              During my free time I am a self-taught learner exploring deeper
+              into topics outside the scope of the college curriculum diving
+              into practice and creating my personal projects. I'm big on
+              keeping my code clean with methodologies like BEM and am always up
+              for learning something new.
+              <br />
+              <br />
+              People often describe me as a motivated and organized person. I
+              like setting goals for projects and making sure I hit them. I
+              believe in bringing out the best results for the companies I work
+              with, all while keeping things dynamic and enjoyable.
+              <br />
+              <br />
+              If you ever want to chat about coding, challenges, or just share
+              some cool ideas, I'm all ears! Let's keep making things better
+              together.
+            </p>
+            {/*
+        TODO: Add achievements in the future
+        <div className="flex flex-row">
             <h1>Over Projects Completed</h1>
             <h1>Freelance Commissions</h1>
             <h1>Years of Experience</h1>
           </div> */}
-        {/* </motion.div> */}
+            {/* </motion.div> */}
+          </div>
+        </div>
       </div>
     </div>
   );
