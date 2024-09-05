1e1868b0022cadaa1f1718a593c63d0767fae0bd
Andrei <andreiwork25@gmail.com>
2024-09-05 16:10:40 +0800

    Update Testimonials.tsx

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -8,7 +8,7 @@ export default function Testimonials() {
       id="testimonials"
       className=" flex w-full flex-col items-center justify-center gap-[50px] bg-background-2  text-center"
     >
-      <div className="flex w-full max-w-[1980px] flex-col justify-between gap-[50px] px-[8px] py-[50px] md:flex-row md:px-[150px] md:py-[200px]">
+      <div className="flex w-full max-w-[1980px] flex-col items-center justify-between gap-[50px] px-[8px] py-[50px] md:px-[150px] md:py-[200px]">
         <h1 className="font-boldtext-left w-full text-[32px] font-bold capitalize text-gray md:text-[64px]">
           What Do My Clients Think?
         </h1>
@@ -17,7 +17,7 @@ export default function Testimonials() {
             ({ id, avatar, name, location, testimonial }) => (
               <li
                 key={id}
-                className="group relative flex h-fit min-h-[250px] w-[400px] flex-col items-center justify-center gap-[24px] rounded-[20px] bg-glass p-10 text-content transition-all duration-500 ease-in-out first:bg-heading first:text-white hover:-translate-y-4"
+                className="group relative flex h-fit min-h-[250px] w-[400px] select-none flex-col items-center justify-center gap-[24px] rounded-[20px] bg-glass p-10 text-content transition-all duration-500 ease-in-out first:bg-heading first:text-white  hover:-translate-y-4"
               >
                 <div className="flex w-full flex-row items-center justify-between">
                   <div className="flex w-full flex-row items-center justify-start gap-[10px]">
@@ -26,7 +26,7 @@ export default function Testimonials() {
                       width={50}
                       height={50}
                       alt="Avatar"
-                      className="h-[50px] w-[50px] select-none overflow-hidden rounded-full bg-background-1"
+                      className="h-[50px] w-[50px] overflow-hidden rounded-full bg-background-1"
                     />
                     <div className="text-left">
                       <h1 className="text-[20px] font-bold text-heading group-first:text-background-1">
@@ -37,9 +37,7 @@ export default function Testimonials() {
                       </p>
                     </div>
                   </div>
-                  <p className="flex select-none justify-end text-sm">
-                    ⭐⭐⭐⭐⭐
-                  </p>
+                  <p className="flex justify-end text-sm">⭐⭐⭐⭐⭐</p>
                 </div>
                 <p className="flex flex-row gap-4 text-left text-[16px] font-semibold text-content group-first:text-background-1">
                   ❝{testimonial}❞
