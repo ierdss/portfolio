bf0eb26aee64bfa2d02308a68f8f02be2f09199d
Andrei <andreiwork25@gmail.com>
2024-09-06 00:08:02 +0800

    Experience section is complete

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,38 +1,118 @@
-import React from "react";
-
-import { VerticalTimeline } from "react-vertical-timeline-component";
+import React, { ReactNode } from "react";
+import Image from "next/image";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
 import { ExperienceSectionContent } from "@/constants";
-import ExperienceCard from "./ExperienceCard";
 
-export default function Experience() {
+export default function Experiences() {
   return (
     <div
       id="experience"
       className="flex w-full flex-col items-center justify-center gap-[50px] bg-background-1"
     >
-      <div className="flex w-full max-w-[1980px] flex-col justify-between gap-[50px] px-[8px] py-[50px] md:flex-row md:px-[150px] md:py-[100px]">
-        <div className="experience__heading">
-          <h1 className="experience__heading-subtitle">Experience</h1>
-          <h1 className="experience__heading-title">What Have I Done?</h1>
-        </div>
+      <div className="flex w-full max-w-[1980px] flex-col justify-between gap-[70px] px-[8px] py-[50px] md:px-[150px] md:py-[100px]">
+        <h1 className="z-20 w-full text-center text-[32px] font-bold capitalize text-gray md:text-[64px]">
+          Where Have I Worked?
+        </h1>
 
-        <VerticalTimeline className="experience__timeline" lineColor="#B40041">
+        <VerticalTimeline className="w-full" lineColor="#B40041">
           {ExperienceSectionContent.map(
-            ({ icon, title, company, date, bullets }, index) => (
-              <ExperienceCard
-                key={index}
+            ({ id, icon, title, company, date, bullets, tags }) => (
+              <Experience
+                key={id}
                 icon={icon}
                 title={title}
                 company={company}
                 date={date}
                 bullets={bullets}
+                tags={tags}
               />
             ),
           )}
         </VerticalTimeline>
       </div>
     </div>
+  );
+}
+
+interface IExperience {
+  icon: ReactNode;
+  title: string;
+  company: string;
+  date: string;
+  bullets: IBullet[];
+  tags: ITag[];
+}
+
+interface IBullet {
+  id: number;
+  bullet: string;
+}
+
+interface ITag {
+  id: number;
+  tag: string;
+}
+
+function Experience({
+  icon,
+  title,
+  company,
+  date,
+  bullets,
+  tags,
+}: IExperience) {
+  return (
+    <VerticalTimelineElement
+      className="group w-full transition-all duration-500 ease-in-out hover:-translate-y-4"
+      contentStyle={{
+        background: "transparent",
+        borderBottom: "transparet",
+        color: "#252131",
+      }}
+      contentArrowStyle={{ borderRight: "10px solid #B40041" }}
+      date={date}
+      dateClassName="text-heading text-[16px]"
+      icon={
+        icon || (
+          <div className="flex aspect-square w-full select-none">
+            <Image
+              src="/patterns/intersecting-circles.svg"
+              width={300}
+              height={300}
+              alt="Circle"
+              loading="lazy"
+              sizes="(max-width: 768px) 50vw, 33vw"
+            />
+          </div>
+        )
+      }
+      iconClassName="bg-white"
+    >
+      <h1 className="text-2xl font-bold text-gray">{title}</h1>
+      <h1 className="text-base font-bold uppercase text-secondary-red">
+        {company}
+      </h1>
+      <ul className="mt-4 flex flex-row flex-wrap gap-2 text-gray2">
+        {tags.map(({ id, tag }) => (
+          <li
+            key={id}
+            className="border-gray1 rounded-full border px-4 py-1 text-[13px] font-bold"
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
+      <ul className="ml-4 mt-4 list-disc text-gray2">
+        {bullets.map(({ id, bullet }) => (
+          <li key={id} className="pl-1 text-base font-bold">
+            {bullet}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
   );
 }
--- constants/SectionContent.tsx
+++ constants/SectionContent.tsx
@@ -208,27 +208,111 @@ export const WorkflowSectionContent = [
 
 export const ExperienceSectionContent = [
   {
+    id: 1,
     title: "Next.js Developer",
     company: "Starbucks",
     icon: null,
     date: "March 2020 - April 2021",
     bullets: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      {
+        id: 1,
+        bullet:
+          "Developing and maintaining web applications using React.js and other related technologies.",
+      },
+      {
+        id: 2,
+        bullet:
+          "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      },
+      {
+        id: 3,
+        bullet:
+          "Implementing responsive design and ensuring cross-browser compatibility.",
+      },
+      {
+        id: 4,
+        bullet:
+          "Participating in code reviews and providing constructive feedback to other developers.",
+      },
+    ],
+    tags: [
+      { id: 1, tag: "Next.js" },
+      { id: 2, tag: "Typescript" },
+      { id: 3, tag: "Tailwind" },
+      { id: 4, tag: "CSS" },
+      { id: 5, tag: "HTML" },
     ],
   },
   {
+    id: 2,
     title: "Next.js Developer",
     company: "Starbucks",
     icon: null,
     date: "March 2020 - April 2021",
     bullets: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      {
+        id: 1,
+        bullet:
+          "Developing and maintaining web applications using React.js and other related technologies.",
+      },
+      {
+        id: 2,
+        bullet:
+          "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      },
+      {
+        id: 3,
+        bullet:
+          "Implementing responsive design and ensuring cross-browser compatibility.",
+      },
+      {
+        id: 4,
+        bullet:
+          "Participating in code reviews and providing constructive feedback to other developers.",
+      },
+    ],
+    tags: [
+      { id: 1, tag: "Next.js" },
+      { id: 2, tag: "Typescript" },
+      { id: 3, tag: "Tailwind" },
+      { id: 4, tag: "CSS" },
+      { id: 5, tag: "HTML" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Next.js Developer",
+    company: "Starbucks",
+    icon: null,
+    date: "March 2020 - April 2021",
+    bullets: [
+      {
+        id: 1,
+        bullet:
+          "Developing and maintaining web applications using React.js and other related technologies.",
+      },
+      {
+        id: 2,
+        bullet:
+          "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      },
+      {
+        id: 3,
+        bullet:
+          "Implementing responsive design and ensuring cross-browser compatibility.",
+      },
+      {
+        id: 4,
+        bullet:
+          "Participating in code reviews and providing constructive feedback to other developers.",
+      },
+    ],
+    tags: [
+      { id: 1, tag: "Next.js" },
+      { id: 2, tag: "Typescript" },
+      { id: 3, tag: "Tailwind" },
+      { id: 4, tag: "CSS" },
+      { id: 5, tag: "HTML" },
     ],
   },
 ];
