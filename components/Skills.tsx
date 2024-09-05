9e8d3eaac3f6e12b1e9c208f6bc34ca50342ef43
Andrei <andreiwork25@gmail.com>
2024-09-05 20:28:01 +0800

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,13 @@
-import React, { ReactNode } from "react";
-import { SkillsRow1, SkillsRow2, SkillsRow3 } from "@/constants";
-import { FaHtml5, FaQuestion } from "react-icons/fa";
+import React from "react";
 import Marquee from "react-fast-marquee";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaCss3Alt, FaHtml5, FaReact, FaSass } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiNextdotjs, SiTailwindcss, SiVercel } from "react-icons/si";
 
 export default function Technologies() {
+  const iconSize = 50;
+
   return (
     <div
       id="technologies"
@@ -17,9 +21,7 @@ export default function Technologies() {
 
         {/* Skill Stack */}
         <div className="z-20 flex max-w-[1980px] flex-col gap-[8px] md:gap-[40px]">
-          {/* <SkillsRow row={SkillsRow1} direction="left" />
-          <SkillsRow row={SkillsRow2} direction="right" />
-          <SkillsRow row={SkillsRow3} direction="left" /> */}
+          {/* Skills Row 1 */}
           <Marquee
             play={true}
             direction={"left"}
@@ -32,13 +34,116 @@ export default function Technologies() {
             <ul className="first-letter: z-20 mr-3 flex w-full flex-row gap-3 md:mr-5 md:gap-5">
               <div className="overflow-hidden rounded-[20px]">
                 <a
-                  key={1}
-                  href={"/"}
+                  href="https://www.w3schools.com/html/default.asp"
                   target="_blank"
                   className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-html text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-html"
                 >
                   <FaHtml5 size={48} />
                   <h1 className="bg-clip-text text-[32px] font-bold">HTML</h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://www.w3schools.com/css/default.asp"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-css text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-css"
+                >
+                  <FaCss3Alt size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">CSS</h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://tailwindcss.com/docs/installation"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-tailwind text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-tailwind"
+                >
+                  <SiTailwindcss size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">
+                    Tailwind
+                  </h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://sass-lang.com/documentation/"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-sass text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-sass"
+                >
+                  <FaSass size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">Sass</h1>
+                </a>
+              </div>
+            </ul>
+          </Marquee>
+
+          {/* Skills Row 2 */}
+          <Marquee
+            play={true}
+            direction={"right"}
+            loop={0}
+            pauseOnHover={false}
+            speed={100}
+            autoFill={true}
+            className="overflow-visible"
+          >
+            <ul className="first-letter: z-20 mr-3 flex w-full flex-row gap-3 md:mr-5 md:gap-5">
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://www.w3schools.com/js/default.asp"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-javascript text-blackberry transition-all duration-300 ease-out hover:scale-150 hover:bg-blackberry hover:text-javascript"
+                >
+                  <IoLogoJavascript size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">
+                    Javascript
+                  </h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://www.typescriptlang.org/docs/"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-typescript text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-typescript"
+                >
+                  <BiLogoTypescript size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">
+                    Typescript
+                  </h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://react.dev/reference/react"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-react text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-react"
+                >
+                  <FaReact size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">
+                    React.JS
+                  </h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://nextjs.org/docs"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-next text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-next"
+                >
+                  <SiNextdotjs size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">
+                    Next.js
+                  </h1>
+                </a>
+              </div>
+              <div className="overflow-hidden rounded-[20px]">
+                <a
+                  href="https://vercel.com/docs"
+                  target="_blank"
+                  className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 bg-next text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-next"
+                >
+                  <SiVercel size={48} />
+                  <h1 className="bg-clip-text text-[32px] font-bold">Vercel</h1>
                 </a>
               </div>
             </ul>
@@ -51,45 +156,5 @@ export default function Technologies() {
         <div className="bg-accent-3 absolute bottom-0 right-0 z-10 h-[650px] w-[650px] translate-x-[30%] translate-y-[30%] rounded-full blur-[300px]" />
       </div>
     </div>
-  );
-}
-
-interface ISkillsRow {
-  row: ISkillsRow[];
-  direction: "left" | "right" | "up" | "down" | undefined;
-}
-
-interface ISkillsRow {
-  id: number;
-  icon: ReactNode;
-  name: string;
-  color: string;
-  link: string;
-}
-
-function SkillsRow({ row, direction }: ISkillsRow) {
-  return (
-    <Marquee
-      play={true}
-      direction={direction}
-      loop={0}
-      pauseOnHover={false}
-      speed={100}
-      autoFill={true}
-    >
-      <ul className="first-letter: z-20 mr-3  flex w-full flex-row gap-3 md:mr-5 md:gap-5">
-        {row.map(({ id, icon, name, color, link }) => (
-          <a
-            key={id}
-            href={link || "/"}
-            target="_blank"
-            className="flex aspect-16/11 h-[220px] w-[480px] flex-row flex-wrap items-center justify-center gap-4 rounded-2xl transition-all duration-300 ease-out hover:scale-105"
-          >
-            <div>{icon || <FaQuestion size={48} />}</div>
-            <h1 className="text-[32px] font-bold">{name || "Name"}</h1>
-          </a>
-        ))}
-      </ul>
-    </Marquee>
   );
 }
