66cdb1edcf24620f47313080ba6c12d39693d25f
Andrei <andreiwork25@gmail.com>
2024-09-06 15:41:15 +0800

    Experience section completed

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from "react";
-import Image from "next/image";
+import React from "react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { Experiences as ExperiencesContent } from "@/constants";
+import { FaStar } from "react-icons/fa";
 
 export default function Experiences() {
   return (
@@ -20,99 +20,56 @@ export default function Experiences() {
 
         <VerticalTimeline className="w-full" lineColor="#B40041">
           {ExperiencesContent.map(
-            ({ id, icon, title, company, date, bullets, tags }) => (
-              <Experience
+            ({ id, icon, iconStyle, title, subtitle, date, bullets, tags }) => (
+              <VerticalTimelineElement
                 key={id}
-                icon={icon}
-                title={title}
-                company={company}
+                className="group relative w-full transition-all duration-500 ease-in-out hover:-translate-y-4"
+                contentStyle={{
+                  background: "transparent",
+                  color: "#252131",
+                }}
+                contentArrowStyle={{ borderRight: "10px solid #B40041" }}
                 date={date}
-                bullets={bullets}
-                tags={tags}
-              />
+                dateClassName="text-heading text-[16px]"
+                icon={icon}
+                iconStyle={iconStyle}
+                iconClassName="scale-150"
+                visible={true}
+              >
+                <h1 className="text-2xl font-bold text-gray">{title}</h1>
+                <h1 className="text-base font-bold uppercase text-secondary-red">
+                  {subtitle}
+                </h1>
+                <ul className="mt-4 flex flex-row flex-wrap gap-2 text-gray2">
+                  {tags.map(({ id, tag }) => (
+                    <li
+                      key={id}
+                      className="border-gray1 rounded-full border px-4 py-1 text-[13px] font-bold"
+                    >
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+                <ul className="ml-4 mt-4 list-disc text-gray2">
+                  {bullets.map(({ id, bullet }) => (
+                    <li key={id} className="pl-1 text-sm">
+                      {bullet}
+                    </li>
+                  ))}
+                </ul>
+                <span className="ease absolute bottom-0 left-[50%] z-20 h-0 w-0 -translate-x-1/2 border-t-2 border-secondary-red transition-all duration-700 group-hover:w-full" />
+                <div className="absolute -bottom-1 left-0 z-10 h-2 w-full bg-blackberry" />
+              </VerticalTimelineElement>
             ),
           )}
+          {/* TODO: Make this add new elements on click in the future when I have more job experience. */}
+          <VerticalTimelineElement
+            iconStyle={{ background: "#B40041", color: "#fff" }}
+            icon={<FaStar />}
+            visible={true}
+          />
         </VerticalTimeline>
       </div>
     </div>
-  );
-}
-
-interface IExperience {
-  icon: ReactNode;
-  title: string;
-  company: string;
-  date: string;
-  bullets: IBullet[];
-  tags: ITag[];
-}
-
-interface IBullet {
-  id: number;
-  bullet: string;
-}
-
-interface ITag {
-  id: number;
-  tag: string;
-}
-
-function Experience({
-  icon,
-  title,
-  company,
-  date,
-  bullets,
-  tags,
-}: IExperience) {
-  return (
-    <VerticalTimelineElement
-      className="group w-full transition-all duration-500 ease-in-out hover:-translate-y-4"
-      contentStyle={{
-        background: "transparent",
-        borderBottom: "transparet",
-        color: "#252131",
-      }}
-      contentArrowStyle={{ borderRight: "10px solid #B40041" }}
-      date={date}
-      dateClassName="text-heading text-[16px]"
-      icon={
-        icon || (
-          <div className="flex aspect-square w-full select-none">
-            <Image
-              src="/patterns/intersecting-circles.svg"
-              width={300}
-              height={300}
-              alt="Circle"
-              loading="lazy"
-              sizes="(max-width: 768px) 50vw, 33vw"
-            />
-          </div>
-        )
-      }
-      iconClassName="bg-white"
-    >
-      <h1 className="text-2xl font-bold text-gray">{title}</h1>
-      <h1 className="text-base font-bold uppercase text-secondary-red">
-        {company}
-      </h1>
-      <ul className="mt-4 flex flex-row flex-wrap gap-2 text-gray2">
-        {tags.map(({ id, tag }) => (
-          <li
-            key={id}
-            className="border-gray1 rounded-full border px-4 py-1 text-[13px] font-bold"
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
-      <ul className="ml-4 mt-4 list-disc text-gray2">
-        {bullets.map(({ id, bullet }) => (
-          <li key={id} className="pl-1 text-base font-bold">
-            {bullet}
-          </li>
-        ))}
-      </ul>
-    </VerticalTimelineElement>
   );
 }
