53b4758c4c9d427bdf891a90ab232a3359d6c3e5
Andrei <andreiwork25@gmail.com>
2024-09-06 18:32:35 +0800

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -19,7 +19,7 @@ export default function Skills() {
       id="skills"
       className="flex w-full flex-col items-center justify-center overflow-hidden bg-background-2"
     >
-      <div className="relative flex w-full max-w-[3000px] flex-col items-center justify-between gap-[70px] px-[8px] py-[50px] md:px-[150px] md:py-[200px]">
+      <div className="relative flex w-[3000px] w-full flex-col items-center justify-between gap-[70px] px-[8px] py-[50px] md:px-[150px] md:py-[200px]">
         <h1 className="z-20 w-full text-center text-[32px] font-bold capitalize text-gray md:text-[64px]">
           The Technologies I Use
           <br className="hidden md:flex" /> To Create My Works
