962618b754322abe05b091755fecbe694efacde9
Andrei <andreiwork25@gmail.com>
2024-09-07 17:57:23 +0800

    Update SocialLinksDB.tsx

--- constants/SocialLinksDB.tsx
+++ constants/SocialLinksDB.tsx
@@ -6,25 +6,21 @@ export const SocialLinksData = [
   {
     id: 1,
     title: "Gmail",
-    icon: <BsMailbox2 className="h-[28px] w-[28px] md:h-[48px] md:w-[48px]" />,
+    icon: <BsMailbox2 className="h-7 w-7 md:h-12 md:w-12" />,
     ariaLabel: "Send me an email!",
     url: "mailto:andreiwork25@gmail.com",
   },
   {
     id: 2,
     title: "LinkedIn",
-    icon: (
-      <RiLinkedinFill className="h-[28px] w-[28px] md:h-[48px] md:w-[48px]" />
-    ),
+    icon: <RiLinkedinFill className="h-7 w-7 md:h-12 md:w-12" />,
     ariaLabel: "Take a look at my work profile on LinkedIn.",
     url: "https://www.linkedin.com/in/andrei-sager-34a452265/",
   },
   {
     id: 3,
     title: "Github",
-    icon: (
-      <TbBrandGithubFilled className="h-[28px] w-[28px] md:h-[48px] md:w-[48px]" />
-    ),
+    icon: <TbBrandGithubFilled className="h-7 w-7 md:h-12 md:w-12" />,
     ariaLabel: "Check out my projects from my GitHub repository!",
     url: "https://github.com/AndreiSager",
   },
