157cb90282748793b5ce0b1af8d5f7180f157ae7
Andrei <andreiwork25@gmail.com>
2024-09-07 17:32:07 +0800

    Update Testimonials.tsx

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -9,15 +9,15 @@ export default function Testimonials() {
   return (
     <div
       id="testimonials"
-      className=" flex w-full flex-col items-center justify-center gap-[50px] overflow-hidden bg-background-2 text-center"
+      className=" flex w-full flex-col items-center justify-center gap-12 overflow-hidden bg-background-2 text-center"
     >
-      <div className="max-w-center relative flex w-full flex-col items-center justify-between gap-[35px] px-[8px] py-[50px] md:gap-[70px] md:px-[150px] md:py-[200px]">
+      <div className="max-w-center relative flex w-full flex-col items-center justify-between gap-9 px-2 py-12 md:gap-20 md:px-40 md:py-48">
         <h2 className="z-20 w-full text-center text-gray">
           What Do My Clients Think?
         </h2>
 
         {/* Testimonials Stack */}
-        <div className="flex flex-col items-center justify-between gap-[8px] md:gap-[40px]">
+        <div className="flex flex-col items-center justify-between gap-2 md:gap-10">
           <TestimonialsRow row={TestimonialsRow1} />
           <TestimonialsRow row={TestimonialsRow2} />
           <TestimonialsRow row={TestimonialsRow3} />
@@ -52,33 +52,33 @@ interface ITestimonial {
 
 function TestimonialsRow({ row }: ITestimonialsRow) {
   return (
-    <ul className="z-20 flex flex-col items-start justify-center gap-[8px] md:flex-row md:gap-[40px]">
+    <ul className="z-20 flex flex-col items-start justify-center gap-2 md:flex-row md:gap-10">
       {row.map(({ id, avatar, name, location, testimonial }) => (
         <li
           key={id}
-          className="group relative flex w-full select-none flex-col items-center justify-start gap-[24px] rounded-[20px] bg-glass p-5 text-content transition-all duration-500 ease-in-out hover:-translate-y-1 md:min-h-[220px] md:w-[400px] md:p-10 md:hover:-translate-y-4"
+          className="group relative flex w-full select-none flex-col items-center justify-start gap-5 rounded-[20px] bg-glass p-5 text-content transition-all duration-500 ease-in-out hover:-translate-y-1 md:min-h-[220px] md:w-[400px] md:p-10 md:hover:-translate-y-4"
         >
           <div className="flex w-full flex-row items-center justify-between">
-            <div className="flex w-full flex-row items-center justify-start gap-[10px]">
-              <div className="h-[50px] w-[50px] overflow-hidden rounded-full bg-gray">
+            <div className="flex w-full flex-row items-center justify-start gap-3">
+              <div className="h-12 w-12 overflow-hidden rounded-full bg-gray">
                 {/* <Image
                   src={avatar}
                   width={50}
                   height={50}
                   alt="Avatar"
-                  className="h-[50px] w-[50px] overflow-hidden rounded-full bg-background-1"
+                  className="h-12 w-12 overflow-hidden rounded-full bg-background-1"
                 /> */}
               </div>
               <div className="text-left">
-                <h6 className="text-[16px] font-bold text-heading">{name}</h6>
-                <p className="font-regular text-[12px] capitalize text-heading">
+                <h6 className="text-base font-bold text-heading">{name}</h6>
+                <p className="font-regular text-xs capitalize text-heading">
                   {location}
                 </p>
               </div>
             </div>
             <p className="flex justify-end text-sm">⭐⭐⭐⭐⭐</p>
           </div>
-          <p className="flex w-full flex-row gap-4 text-left text-[13px] font-semibold text-content">
+          <p className="flex w-full flex-row gap-4 text-left text-xs font-semibold text-content">
             ❝{testimonial}❞
           </p>
         </li>
