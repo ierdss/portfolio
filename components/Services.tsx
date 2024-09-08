94b0664b2385fb4af7116a0f938b7e84049144bb
Andrei <andreiwork25@gmail.com>
2024-09-08 18:53:22 +0800

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -10,7 +10,7 @@ export default function Services() {
     >
       <div className="relative flex w-full max-w-center flex-col items-center justify-between gap-20 px-2 py-12 md:px-40 md:py-48">
         <div className="z-20 flex w-full flex-col gap-12 md:gap-20 ">
-          <h1 className="z-20 w-full text-left text-gray">What Do I Offer?</h1>
+          <h1 className="text-text z-20 w-full text-left">What Do I Offer?</h1>
           <ul className="flex w-full flex-col flex-wrap items-center justify-between gap-2 md:flex-row">
             {ServicesData.map(({ id, icon, title, description }) => (
               <ServiceCard
@@ -24,7 +24,7 @@ export default function Services() {
         </div>
 
         <div className="z-20 flex flex-col gap-12 md:gap-20 ">
-          <h2 className="text-center text-gray">How Do I Get The Job Done?</h2>
+          <h2 className="text-text text-center">How Do I Get The Job Done?</h2>
           <ul className="flex w-full flex-col flex-wrap items-center justify-between gap-2 md:flex-row md:gap-24">
             {WorkflowData.map(({ id, number, title, description }) => (
               <WorkflowCard
@@ -39,13 +39,13 @@ export default function Services() {
 
         {/* Background Mist Effects */}
         <div className="absolute left-0 top-0 -translate-x-[30%] -translate-y-[30%]">
-          <div className="md:animate-grow-shrink-1 z-10 h-[650px] w-[650px] bg-accent-1 blur-[300px] " />
+          <div className="z-10 h-[650px] w-[650px] bg-accent-1 blur-[300px] md:animate-grow-shrink-1 " />
         </div>
         <div className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]">
-          <div className="md:animate-grow-shrink-2 z-10 h-[650px] w-[650px] bg-accent-2 blur-[300px]" />
+          <div className="z-10 h-[650px] w-[650px] bg-accent-2 blur-[300px] md:animate-grow-shrink-2" />
         </div>
         <div className="absolute bottom-0 right-0 translate-x-[30%] translate-y-[30%]">
-          <div className="md:animate-grow-shrink-3 z-10 h-[650px] w-[650px] bg-accent-3 blur-[300px]" />
+          <div className="z-10 h-[650px] w-[650px] bg-accent-3 blur-[300px] md:animate-grow-shrink-3" />
         </div>
       </div>
     </div>
@@ -62,18 +62,18 @@ function ServiceCard({ icon, title, description }: IService) {
   return (
     <div
       className={
-        "group z-20 flex w-full flex-row gap-6 p-6 transition-all duration-300 ease-out hover:scale-105 hover:text-secondary-red md:min-h-[200px] md:max-w-[360px]"
+        "hover:text-primary group z-20 flex w-full flex-row gap-6 p-6 transition-all duration-300 ease-out hover:scale-105 md:min-h-[200px] md:max-w-[360px]"
       }
     >
       <div className="relative z-10 flex h-12 w-12">
-        <div className="z-20 h-12 w-12 text-secondary-red group-hover:text-gray">
+        <div className="text-primary group-hover:text-text z-20 h-12 w-12">
           {icon || <BsQuestionOctagon size={48} />}
         </div>
         <div className="absolute left-[50%] top-[50%] z-10 h-0 w-0 -translate-x-[50%] -translate-y-[50%] rounded-full bg-redberry duration-500 ease-in-out group-hover:h-20 group-hover:w-20" />
       </div>
       <div className="flex flex-col gap-2">
-        <h6 className="text-left text-gray">{title || "Title"}</h6>
-        <p className="text-left text-sm normal-case text-gray">
+        <h6 className="text-text text-left">{title || "Title"}</h6>
+        <p className="text-text text-left text-sm normal-case">
           {description ||
             "Lorem ipsum amet consectetur, adipisicing elit. Tenetur error, rem officia?"}
         </p>
@@ -92,13 +92,13 @@ function WorkflowCard({ number, title, description }: IWorkflow) {
   return (
     <div
       className={
-        "group z-20 flex w-full flex-row items-center justify-center gap-4 p-6 transition-all duration-300 ease-out hover:scale-105 hover:text-secondary-red md:max-h-[250px] md:min-h-[200px] md:max-w-[700px] md:gap-16 [&:nth-child(2)]:flex-row-reverse md:[&:nth-child(2)]:flex-row md:[&:nth-child(3)]:flex-row-reverse [&:nth-child(4)]:flex-row-reverse"
+        "hover:text-primary group z-20 flex w-full flex-row items-center justify-center gap-4 p-6 transition-all duration-300 ease-out hover:scale-105 md:max-h-[250px] md:min-h-[200px] md:max-w-[700px] md:gap-16 [&:nth-child(2)]:flex-row-reverse md:[&:nth-child(2)]:flex-row md:[&:nth-child(3)]:flex-row-reverse [&:nth-child(4)]:flex-row-reverse"
       }
     >
-      <h1 className="numbers select-none text-redberry">{number}</h1>
+      <h1 className="numbers text-primary select-none">{number}</h1>
       <div className="flex flex-col gap-2">
-        <h6 className="text-left text-gray">{title || "Title"}</h6>
-        <p className="text-left text-sm normal-case text-gray">
+        <h6 className="text-text text-left">{title || "Title"}</h6>
+        <p className="text-text text-left text-sm normal-case">
           {description ||
             "Lorem ipsum amet consectetur, adipisicing elit. Tenetur error, rem officia?"}
         </p>
