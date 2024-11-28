f56d1811b95146e521dc84c318e4eeb192e6cb7c
Andrei <andreiwork25@gmail.com>
2024-11-28 12:18:06 +0800

    Removed ids from maps

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,15 +16,18 @@ export default function Experiences() {
       className="flex w-full flex-col items-center justify-center bg-background-1"
     >
       <div className="flex w-full max-w-center flex-col justify-between gap-12 overflow-hidden px-2 py-12 md:gap-20 md:px-40 md:py-24">
-        <h2 className="text-text z-20 w-full text-center">
+        <h2 className="z-20 w-full text-center text-text">
           Where Have I Worked?
         </h2>
 
         <VerticalTimeline className="w-full" lineColor="#B40041">
           {ExperiencesData.map(
-            ({ id, icon, iconStyle, title, subtitle, date, bullets, tags }) => (
+            (
+              { icon, iconStyle, title, subtitle, date, bullets, tags },
+              index,
+            ) => (
               <VerticalTimelineElement
-                key={id}
+                key={index}
                 className="group relative w-full transition-all duration-500 ease-in-out hover:-translate-y-4"
                 contentStyle={{
                   background: "transparent",
@@ -38,28 +41,28 @@ export default function Experiences() {
                 iconClassName="scale-150"
                 visible={testingVisibility}
               >
-                <h4 className="text-text text-2xl font-bold">{title}</h4>
-                <h6 className="text-primary text-base font-bold uppercase">
+                <h4 className="text-2xl font-bold text-text">{title}</h4>
+                <h6 className="text-base font-bold uppercase text-primary">
                   {subtitle}
                 </h6>
-                <ul className="text-text mt-4 flex flex-row flex-wrap gap-2">
-                  {tags.map(({ id, tag }) => (
+                <ul className="mt-4 flex flex-row flex-wrap gap-2 text-text">
+                  {tags.map((item, index) => (
                     <li
-                      key={id}
-                      className="border-text rounded-full border px-4 py-1 text-xs"
+                      key={index}
+                      className="rounded-full border border-text px-4 py-1 text-xs"
                     >
-                      {tag}
+                      {item}
                     </li>
                   ))}
                 </ul>
-                <ul className="text-text ml-4 mt-4 list-disc">
-                  {bullets.map(({ id, bullet }) => (
-                    <li key={id} className="pl-1 text-sm">
-                      {bullet}
+                <ul className="ml-4 mt-4 list-disc text-text">
+                  {bullets.map((item, index) => (
+                    <li key={index} className="pl-1 text-sm">
+                      {item}
                     </li>
                   ))}
                 </ul>
-                <span className="ease border-primary absolute bottom-0 left-[50%] z-20 h-0 w-0 -translate-x-1/2 border-t-2 transition-all duration-700 group-hover:w-full" />
+                <span className="ease absolute bottom-0 left-[50%] z-20 h-0 w-0 -translate-x-1/2 border-t-2 border-primary transition-all duration-700 group-hover:w-full" />
                 <div className="absolute -bottom-1 left-0 z-10 h-2 w-full bg-background-1" />
                 <div className="absolute -left-1 top-0 z-10 h-full w-2 bg-background-1" />
                 <div className="absolute -right-1 top-0 z-10 h-full w-2 bg-background-1" />
--- constants/ExperienceDB.tsx
+++ constants/ExperienceDB.tsx
@@ -3,145 +3,161 @@ import { SiNextdotjs } from "react-icons/si";
 
 export const ExperiencesData = [
   {
-    id: 1,
     icon: <SiNextdotjs className="scale-150" />,
     iconStyle: { background: "#252131", color: "#F4F5F8" },
     title: "Next.js Developer",
     subtitle: "TechLoom",
     date: "March 2024 - Present",
-    tags: [
-      { id: 1, tag: "Next.js" },
-      { id: 2, tag: "Typescript" },
-      { id: 3, tag: "Tailwind" },
-      { id: 4, tag: "CSS" },
-      { id: 5, tag: "HTML" },
-    ],
+    tags: ["Next.js", "Typescript", "Tailwind", "CSS", "HTML"],
     bullets: [
-      {
-        id: 1,
-        bullet:
-          "Developed and maintained custom web applications using Next.js to improve user experiences for TechLoom’s SaaS products.",
-      },
-      {
-        id: 2,
-        bullet:
-          "Collaborated with cross-functional teams including designers, product managers, and developers to create high-quality, user-centered features.",
-      },
-      {
-        id: 3,
-        bullet:
-          "Implemented responsive design and optimized applications for cross-browser compatibility and performance.",
-      },
-      {
-        id: 4,
-        bullet:
-          "Participated in code reviews, providing feedback to ensure clean, maintainable, and efficient code.",
-      },
-      {
-        id: 5,
-        bullet:
-          "Leveraged Next.js’s SSR capabilities to enhance SEO and load times for client-facing products.",
-      },
-      {
-        id: 6,
-        bullet:
-          "Integrated third-party APIs to deliver real-time data updates and interactive user experiences.",
-      },
+      "Developed and maintained custom web applications using Next.js to improve user experiences for TechLoom’s SaaS products.",
+      "Collaborated with cross-functional teams including designers, product managers, and developers to create high-quality, user-centered features.",
+      "Implemented responsive design and optimized applications for cross-browser compatibility and performance.",
+      "Participated in code reviews, providing feedback to ensure clean, maintainable, and efficient code.",
+      "Leveraged Next.js’s SSR capabilities to enhance SEO and load times for client-facing products.",
+      "Integrated third-party APIs to deliver real-time data updates and interactive user experiences.",
     ],
   },
-  {
-    id: 2,
-    icon: <SiNextdotjs className="scale-150" />,
-    iconStyle: { background: "#252131", color: "#F4F5F8" },
-    title: "Next.js Developer",
-    subtitle: "Starbucks",
-    date: "May 2022 - February 2024",
-    tags: [
-      { id: 1, tag: "Next.js" },
-      { id: 2, tag: "Javascript" },
-      { id: 3, tag: "Tailwind" },
-      { id: 4, tag: "Sass" },
-      { id: 5, tag: "HTML" },
-    ],
-    bullets: [
-      {
-        id: 1,
-        bullet:
-          "Led the development of web applications using Next.js for CodeNimbus’s digital solutions, focusing on speed and scalability.",
-      },
-      {
-        id: 2,
-        bullet:
-          "Worked closely with UI/UX designers to implement pixel-perfect user interfaces using Tailwind and Sass.",
-      },
-      {
-        id: 3,
-        bullet:
-          "Optimized web applications for SEO and performance through server-side rendering and efficient code practices.",
-      },
-      {
-        id: 4,
-        bullet:
-          "Integrated third-party services and APIs, providing enhanced functionalities and seamless user experiences.",
-      },
-      {
-        id: 5,
-        bullet:
-          "Collaborated in an agile environment, participating in sprint planning and contributing to the timely delivery of key features.",
-      },
-      {
-        id: 6,
-        bullet:
-          "Mentored junior developers on best practices in front-end development, improving overall team productivity.",
-      },
-    ],
-  },
+  // {
+  //   id: 1,
+  //   icon: <SiNextdotjs className="scale-150" />,
+  //   iconStyle: { background: "#252131", color: "#F4F5F8" },
+  //   title: "Next.js Developer",
+  //   subtitle: "TechLoom",
+  //   date: "March 2024 - Present",
+  //   tags: [
+  //     { id: 1, tag: "Next.js" },
+  //     { id: 2, tag: "Typescript" },
+  //     { id: 3, tag: "Tailwind" },
+  //     { id: 4, tag: "CSS" },
+  //     { id: 5, tag: "HTML" },
+  //   ],
+  //   bullets: [
+  //     {
+  //       id: 1,
+  //       bullet:
+  //         "Developed and maintained custom web applications using Next.js to improve user experiences for TechLoom’s SaaS products.",
+  //     },
+  //     {
+  //       id: 2,
+  //       bullet:
+  //         "Collaborated with cross-functional teams including designers, product managers, and developers to create high-quality, user-centered features.",
+  //     },
+  //     {
+  //       id: 3,
+  //       bullet:
+  //         "Implemented responsive design and optimized applications for cross-browser compatibility and performance.",
+  //     },
+  //     {
+  //       id: 4,
+  //       bullet:
+  //         "Participated in code reviews, providing feedback to ensure clean, maintainable, and efficient code.",
+  //     },
+  //     {
+  //       id: 5,
+  //       bullet:
+  //         "Leveraged Next.js’s SSR capabilities to enhance SEO and load times for client-facing products.",
+  //     },
+  //     {
+  //       id: 6,
+  //       bullet:
+  //         "Integrated third-party APIs to deliver real-time data updates and interactive user experiences.",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   icon: <SiNextdotjs className="scale-150" />,
+  //   iconStyle: { background: "#252131", color: "#F4F5F8" },
+  //   title: "Next.js Developer",
+  //   subtitle: "Starbucks",
+  //   date: "May 2022 - February 2024",
+  //   tags: [
+  //     { id: 1, tag: "Next.js" },
+  //     { id: 2, tag: "Javascript" },
+  //     { id: 3, tag: "Tailwind" },
+  //     { id: 4, tag: "Sass" },
+  //     { id: 5, tag: "HTML" },
+  //   ],
+  //   bullets: [
+  //     {
+  //       id: 1,
+  //       bullet:
+  //         "Led the development of web applications using Next.js for CodeNimbus’s digital solutions, focusing on speed and scalability.",
+  //     },
+  //     {
+  //       id: 2,
+  //       bullet:
+  //         "Worked closely with UI/UX designers to implement pixel-perfect user interfaces using Tailwind and Sass.",
+  //     },
+  //     {
+  //       id: 3,
+  //       bullet:
+  //         "Optimized web applications for SEO and performance through server-side rendering and efficient code practices.",
+  //     },
+  //     {
+  //       id: 4,
+  //       bullet:
+  //         "Integrated third-party services and APIs, providing enhanced functionalities and seamless user experiences.",
+  //     },
+  //     {
+  //       id: 5,
+  //       bullet:
+  //         "Collaborated in an agile environment, participating in sprint planning and contributing to the timely delivery of key features.",
+  //     },
+  //     {
+  //       id: 6,
+  //       bullet:
+  //         "Mentored junior developers on best practices in front-end development, improving overall team productivity.",
+  //     },
+  //   ],
+  // },
 
-  {
-    id: 3,
-    icon: <FaReact className="scale-150" />,
-    iconStyle: { background: "#61dbfb", color: "#F4F5F8" },
-    title: "React.js Developer",
-    subtitle: "BrightByte",
-    date: "June 2021 - April 2022",
-    tags: [
-      { id: 1, tag: "React.js" },
-      { id: 2, tag: "Redux" },
-      { id: 3, tag: "JavaScript" },
-      { id: 4, tag: "CSS" },
-      { id: 5, tag: "HTML" },
-    ],
-    bullets: [
-      {
-        id: 1,
-        bullet:
-          "Developed and enhanced features for BrightByte’s education management platform using React.js, improving the user experience for educators and students.",
-      },
-      {
-        id: 2,
-        bullet:
-          "Worked with backend engineers to integrate RESTful APIs, ensuring seamless communication between the front-end and back-end systems.",
-      },
-      {
-        id: 3,
-        bullet:
-          "Built reusable React components to speed up development and maintain consistency across different parts of the application.",
-      },
-      {
-        id: 4,
-        bullet:
-          "Focused on performance optimization by using Redux for state management and memoization techniques in React.",
-      },
-      {
-        id: 5,
-        bullet:
-          "Implemented unit tests with Jest to ensure stability and reliability in key application components.",
-      },
-      {
-        id: 6,
-        bullet:
-          "Collaborated with designers to ensure compliance with accessibility and design guidelines.",
-      },
-    ],
-  },
+  // {
+  //   id: 3,
+  //   icon: <FaReact className="scale-150" />,
+  //   iconStyle: { background: "#61dbfb", color: "#F4F5F8" },
+  //   title: "React.js Developer",
+  //   subtitle: "BrightByte",
+  //   date: "June 2021 - April 2022",
+  //   tags: [
+  //     { id: 1, tag: "React.js" },
+  //     { id: 2, tag: "Redux" },
+  //     { id: 3, tag: "JavaScript" },
+  //     { id: 4, tag: "CSS" },
+  //     { id: 5, tag: "HTML" },
+  //   ],
+  //   bullets: [
+  //     {
+  //       id: 1,
+  //       bullet:
+  //         "Developed and enhanced features for BrightByte’s education management platform using React.js, improving the user experience for educators and students.",
+  //     },
+  //     {
+  //       id: 2,
+  //       bullet:
+  //         "Worked with backend engineers to integrate RESTful APIs, ensuring seamless communication between the front-end and back-end systems.",
+  //     },
+  //     {
+  //       id: 3,
+  //       bullet:
+  //         "Built reusable React components to speed up development and maintain consistency across different parts of the application.",
+  //     },
+  //     {
+  //       id: 4,
+  //       bullet:
+  //         "Focused on performance optimization by using Redux for state management and memoization techniques in React.",
+  //     },
+  //     {
+  //       id: 5,
+  //       bullet:
+  //         "Implemented unit tests with Jest to ensure stability and reliability in key application components.",
+  //     },
+  //     {
+  //       id: 6,
+  //       bullet:
+  //         "Collaborated with designers to ensure compliance with accessibility and design guidelines.",
+  //     },
+  //   ],
+  // },
 ];
