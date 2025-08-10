2fd53ec114cbff10bd8cb87083ccfec34aed8705
Andrei <ierdss@proton.me>
2025-08-10 20:49:10 +0800

    style(footer); add screen div

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,12 +13,14 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="relative flex w-full max-w-center flex-col items-center justify-center gap-12 bg-background-1 px-2 py-12 md:flex-row md:px-40 md:py-24"
+      className="relative flex w-full flex-col items-center justify-center gap-12 bg-background-1  md:flex-row "
     >
-      <p className="order-1 flex w-fit flex-row gap-[6px] text-center text-xs text-text md:order-2 md:text-xl">
-        © 2023{" "}
-        <span className="flex h-fit w-fit font-bold"> Andrei Sager</span>
-      </p>
+      <div className="flex w-full max-w-center justify-center px-2 py-12 md:px-40 md:py-24">
+        <p className="flex flex-row gap-[6px] text-xs text-text md:order-2 md:text-xl">
+          © 2023{" "}
+          <span className="flex h-fit w-fit font-bold"> Andrei Sager</span>
+        </p>
+      </div>
     </footer>
   );
 }
