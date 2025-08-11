235c713146528d274e52598f1c95b49de5f81a75
Andrei <ierdss@proton.me>
2025-08-11 13:22:47 +0800

    refac(skill): add dynamic skill cards

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,3 +1,4 @@
+import { SkillData } from "@/constants/SkillDB";
 import React from "react";
 import { BiLogoJavascript, BiLogoSass, BiLogoTypescript } from "react-icons/bi";
 import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
@@ -18,13 +19,30 @@ export default function Skills() {
   return (
     <div
       id="skills"
-      className="j/saustify-center flex w-full flex-col items-center overflow-hidden bg-background-2"
+      className="flex w-full flex-col items-center overflow-hidden bg-background-2"
     >
       <div className="flex w-full max-w-center flex-col gap-12 px-2 py-12 text-left md:gap-20 md:px-40 md:py-48">
         <div className="flex w-full ">
           <Heading1 text="Skills and Technologies" />
         </div>
         <Divider />
+
+        {/* Skill Grid */}
+        <div className="z-20 flex w-full flex-col gap-2 md:gap-10">
+          <ul className="z-20 mr-3 flex w-full flex-row gap-3 md:mr-5 md:gap-5">
+            {SkillData.map(({ href, icon, name }, index) => (
+              <div
+                className="min-h-52 overflow-hidden rounded-[20px]"
+                key={`${index}skill`}
+              >
+                <a href={href} target="_blank" className="skill-card text-html">
+                  {icon}
+                  <h6 className="text-text">{name}</h6>
+                </a>
+              </div>
+            ))}
+          </ul>
+        </div>
 
         {/* Skill Stack */}
         <div className="z-20 flex w-[3000px] flex-col gap-2 [mask-image:radial-gradient(circle,#000_50%,transparent_90%)] md:gap-10">
---
+++ constants/SkillDB.tsx
@@ -0,0 +1,69 @@
+import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
+
+const iconSize = 40;
+
+export const SkillData = [
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+  {
+    href: "https://www.w3schools.com/html/default.asp",
+    icon: (
+      <div className="text-html">
+        <FaHtml5 size={iconSize} />
+      </div>
+    ),
+    name: "HTML",
+  },
+];
