dc5d51792c4bb9be55888fadbe874455f5665f59
Andrei <ierdss@proton.me>
2025-08-11 13:54:35 +0800

    chore(skills): add skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -29,7 +29,7 @@ export default function Skills() {
 
         {/* Skill Grid */}
         <div className="z-20 flex w-full flex-col gap-2 md:gap-10">
-          <ul className="z-20 mr-3 flex w-full flex-row gap-3 md:mr-5 md:gap-5">
+          <ul className="z-20 mr-3 flex w-full flex-row flex-wrap justify-between gap-3 md:mr-5 md:gap-5">
             {SkillData.map(({ href, icon, name }, index) => (
               <div
                 className="min-h-52 overflow-hidden rounded-[20px]"
@@ -47,36 +47,6 @@ export default function Skills() {
         {/* Skill Stack */}
         <div className="z-20 flex w-[3000px] flex-col gap-2 [mask-image:radial-gradient(circle,#000_50%,transparent_90%)] md:gap-10">
           <ul className="first-letter: z-20 mr-3 flex w-full flex-row justify-center gap-3 md:mr-5 md:gap-5">
-            <div className="min-h-52 overflow-hidden rounded-[20px]">
-              <a
-                href="https://www.w3schools.com/html/default.asp"
-                target="_blank"
-                className="skill-card text-html"
-              >
-                <FaHtml5 size={iconSize} />
-                <h6 className="text-black">HTML</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://www.w3schools.com/css/default.asp"
-                target="_blank"
-                className="skill-card text-css"
-              >
-                <FaCss3Alt size={iconSize} />
-                <h6 className="text-black">CSS</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://tailwindcss.com/docs/installation"
-                target="_blank"
-                className="skill-card text-tailwind"
-              >
-                <SiTailwindcss size={iconSize} />
-                <h6 className="text-black">Tailwind</h6>
-              </a>
-            </div>
             <div className="overflow-hidden rounded-[20px]">
               <a
                 href="https://sass-lang.com/documentation/"
@@ -87,116 +57,6 @@ export default function Skills() {
                 <h6 className="text-black">Sass</h6>
               </a>
             </div>
-          </ul>
-
-          {/* Skills Row 2 */}
-          <ul className="first-letter: z-20 mr-3 flex w-full flex-row justify-center gap-3 md:mr-5 md:gap-5">
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://www.w3schools.com/js/default.asp"
-                target="_blank"
-                className="skill-card text-javascript"
-              >
-                <BiLogoJavascript size={iconSize} />
-                <h6 className="text-black">Javascript</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://www.typescriptlang.org/docs/"
-                target="_blank"
-                className="skill-card text-typescript"
-              >
-                <BiLogoTypescript size={iconSize} />
-                <h6 className="text-black">Typescript</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://react.dev/reference/react"
-                target="_blank"
-                className="skill-card text-react"
-              >
-                <FaReact size={iconSize} />
-                <h6 className="text-black">React.JS</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://nextjs.org/docs"
-                target="_blank"
-                className="skill-card text-next"
-              >
-                <SiNextdotjs size={iconSize} />
-                <h6 className="text-black">Next.js</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://vercel.com/docs"
-                target="_blank"
-                className="skill-card text-next"
-              >
-                <SiVercel size={iconSize} />
-                <h6 className="text-black">Vercel</h6>
-              </a>
-            </div>
-          </ul>
-
-          {/* Skills Row 3 */}
-          <ul className="first-letter: z-20 mr-3 flex w-full flex-row justify-center gap-3 md:mr-5 md:gap-5">
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://www.markdownguide.org/"
-                target="_blank"
-                className="skill-card text-blackberry"
-              >
-                <IoLogoMarkdown size={iconSize} />
-                <h6 className="text-black">Markdown</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://obsidian.md/"
-                target="_blank"
-                className="skill-card text-obsidian"
-              >
-                <SiObsidian size={iconSize} />
-                <h6 className="text-black">Obsidian</h6>
-              </a>
-            </div>
-            <div className="overflow-hidden rounded-[20px]">
-              <a
-                href="https://graphql.org/"
-                target="_blank"
-                className="skill-card text-graphql"
-              >
-                <GrGraphQl size={iconSize} />
-                <h6 className="text-black">GraphQl</h6>
-              </a>
-            </div>
-            {/* <div className="overflow-hidden rounded-[20px]">
-                <a
-                  href="https://nextjs.org/docs"
-                  target="_blank"
-                  className="flex aspect-16/11 h-56 w-[30rem] flex-row flex-wrap items-center justify-center gap-4 bg-next text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-next"
-                >
-                  <SiNextdotjs size={iconSize} />
-                  <h4>
-                    Next.js
-                  </h4>
-                </a>
-              </div>
-              <div className="overflow-hidden rounded-[20px]">
-                <a
-                  href="https://vercel.com/docs"
-                  target="_blank"
-                  className="flex aspect-16/11 h-56 w-[30rem] flex-row flex-wrap items-center justify-center gap-4 bg-next text-gray transition-all duration-300 ease-out hover:scale-150 hover:bg-gray hover:text-next"
-                >
-                  <SiVercel size={iconSize} />
-                  <h4>Vercel</h4>
-                </a>
-              </div> */}
           </ul>
         </div>
 
--- constants/SkillDB.tsx
+++ constants/SkillDB.tsx
@@ -1,4 +1,14 @@
-import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
+import { BiLogoJavascript, BiLogoTypescript } from "react-icons/bi";
+import {
+  FaCss3Alt,
+  FaGitAlt,
+  FaGithub,
+  FaHtml5,
+  FaReact,
+} from "react-icons/fa";
+import { IoLogoMarkdown } from "react-icons/io5";
+import { PiFigmaLogoFill } from "react-icons/pi";
+import { SiNextdotjs, SiTailwindcss, SiVercel, SiVim } from "react-icons/si";
 
 const iconSize = 40;
 
@@ -13,57 +23,125 @@ export const SkillData = [
     name: "HTML",
   },
   {
-    href: "https://www.w3schools.com/html/default.asp",
+    href: "https://www.w3schools.com/css/default.asp",
     icon: (
-      <div className="text-html">
-        <FaHtml5 size={iconSize} />
+      <div className="text-css">
+        <FaCss3Alt size={iconSize} />
       </div>
     ),
-    name: "HTML",
+    name: "CSS",
   },
   {
-    href: "https://www.w3schools.com/html/default.asp",
+    href: "https://tailwindcss.com/docs/installation",
     icon: (
-      <div className="text-html">
-        <FaHtml5 size={iconSize} />
+      <div className="text-tailwind">
+        <SiTailwindcss size={iconSize} />
       </div>
     ),
-    name: "HTML",
+    name: "Tailwind",
   },
   {
-    href: "https://www.w3schools.com/html/default.asp",
+    href: "https://www.w3schools.com/js/default.asp",
     icon: (
-      <div className="text-html">
-        <FaHtml5 size={iconSize} />
+      <div className="text-javascript">
+        <BiLogoJavascript size={iconSize} />
       </div>
     ),
-    name: "HTML",
+    name: "JavaScript",
   },
   {
-    href: "https://www.w3schools.com/html/default.asp",
+    href: "https://www.typescriptlang.org/docs/",
     icon: (
-      <div className="text-html">
-        <FaHtml5 size={iconSize} />
+      <div className="text-typescript">
+        <BiLogoTypescript size={iconSize} />
       </div>
     ),
-    name: "HTML",
+    name: "TypeScript",
   },
   {
-    href: "https://www.w3schools.com/html/default.asp",
+    href: "https://react.dev/reference/react",
     icon: (
-      <div className="text-html">
-        <FaHtml5 size={iconSize} />
+      <div className="text-react">
+        <FaReact size={iconSize} />
       </div>
     ),
-    name: "HTML",
+    name: "React",
   },
   {
-    href: "https://www.w3schools.com/html/default.asp",
+    href: "https://nextjs.org/docs",
     icon: (
-      <div className="text-html">
-        <FaHtml5 size={iconSize} />
+      <div className="text-next">
+        <SiNextdotjs size={iconSize} />
       </div>
     ),
-    name: "HTML",
+    name: "Next.js",
+  },
+  {
+    href: "https://vercel.com/docs",
+    icon: (
+      <div className="text-next">
+        <SiVercel size={iconSize} />
+      </div>
+    ),
+    name: "Vercel",
+  },
+  {
+    href: "https://www.markdownguide.org/",
+    icon: (
+      <div className="text-blackberry">
+        <IoLogoMarkdown size={iconSize} />
+      </div>
+    ),
+    name: "Markdown",
+  },
+  {
+    href: "https://git-scm.com/",
+    icon: (
+      <div className="text-git">
+        <FaGitAlt size={iconSize} />
+      </div>
+    ),
+    name: "Git",
+  },
+  {
+    href: "https://github.com/",
+    icon: (
+      <div className="text-github">
+        <FaGithub size={iconSize} />
+      </div>
+    ),
+    name: "GitHub",
+  },
+  {
+    href: "https://www.vim.org/",
+    icon: (
+      <div className="text-vim">
+        <SiVim size={iconSize} />
+      </div>
+    ),
+    name: "Vim",
+  },
+  {
+    href: "",
+    icon: (
+      <div className="text-react">
+        <PiFigmaLogoFill size={iconSize} />
+      </div>
+    ),
+    name: "Figma",
+  },
+  {
+    href: "",
+    icon: <div className="text-react"></div>,
+    name: "",
   },
 ];
+
+//  {
+//     href: "",
+//     icon: (
+//       <div className="text-">
+//       </div>
+//     ),
+//     name: "",
+//   },
