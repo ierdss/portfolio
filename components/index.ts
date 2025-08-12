6cb843609fe44e18b2f7c1d6de965eb071719796
Andrei <ierdss@proton.me>
2025-08-12 12:58:16 +0800

    feat(services): delete component and data

--- components/index.ts
+++ components/index.ts
@@ -3,21 +3,11 @@ import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Projects from "./Projects";
-import Services from "./Services";
 import Skills from "./Skills";
 import Testimonials from "./Testimonials";
 
 // Components
-export {
-  Experience,
-  Footer,
-  Hero,
-  Navbar,
-  Projects,
-  Services,
-  Skills,
-  Testimonials,
-};
+export { Experience, Footer, Hero, Navbar, Projects, Skills, Testimonials };
 
 import Heading1 from "./headings/Heading1";
 
