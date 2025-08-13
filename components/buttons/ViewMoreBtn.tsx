6bff400c6a4849c7daf3097cf9b42f840361430e
Andrei <ierdss@proton.me>
2025-08-13 19:23:20 +0800

    feat: remove z index

--- components/buttons/ViewMoreBtn.tsx
+++ components/buttons/ViewMoreBtn.tsx
@@ -3,7 +3,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export default function ViewMoreBtn({ text, href }: AnchorProps) {
   return (
     <div className="flex flex-row items-center gap-2">
-      <a className="z-20 w-full text-center text-text" href={href}>
+      <a className="w-full text-center text-text" href={href}>
         {text}
       </a>
       <FaArrowRightLong size={28} />
--- components/headings/Heading1.tsx
+++ components/headings/Heading1.tsx
@@ -1,3 +1,3 @@
 export default function Heading1({ text }: HeadingProps) {
-  return <h3 className="z-20 text-text">{text}</h3>;
+  return <h3 className="text-text">{text}</h3>;
 }
