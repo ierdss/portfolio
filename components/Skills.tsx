86098129b4d03ada411fb3a880b5ac1ad8097c87
Andrei <ierdss@proton.me>
2025-08-14 15:35:44 +0800

    style(skills): responsive on all screens

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,7 +7,8 @@ export default function Skills() {
       id="skills"
       className="flex w-full flex-col items-center overflow-hidden bg-background-2"
     >
-      <div className="flex w-full max-w-center flex-col items-center gap-8 px-2 py-12 text-left md:gap-20 md:px-40 md:py-48">
+      <div className="flex w-full max-w-center flex-col items-center gap-8 px-2 py-12 text-left md:py-48">
+        {/* <div className="flex w-full max-w-center flex-col items-center gap-8 px-2 py-12 text-left md:gap-20 md:px-40 md:py-48"> */}
         <div className="flex w-full max-w-screen-xl">
           <Heading1 text="Skills and Technologies" />
         </div>
@@ -15,7 +16,7 @@ export default function Skills() {
 
         {/* Skill Grid */}
         <div className="z-20 flex w-full max-w-screen-xl flex-col gap-2 md:gap-10">
-          <ul className="smd:grid-cols-4 z-20 grid w-full grid-cols-3 place-items-center justify-items-center md:mr-5 md:gap-16">
+          <ul className="smd:grid-cols-4 z-20 grid w-full grid-cols-3 place-items-center justify-items-center md:mr-5 md:grid-cols-5 md:gap-16 lg:grid-cols-7 xl:grid-cols-8">
             {SkillsData.map(({ href, icon, name }, index) => (
               <a
                 href={href}
