08da44f5d5eca9fa343e922a24ed5deb883bca07
Andrei <ierdss@proton.me>
2025-08-14 12:54:36 +0800

    chore(hero): fix line break

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,8 +45,7 @@ export default function Hero() {
           <p>
             A 25 y/o Frontend Web Developer based in the Philippines
             <br className="hidden md:flex" /> specializing in Landing Pages &
-            <br className="md:hidden" />
-            E-commerce Websites
+            <br className="md:hidden" /> E-commerce Websites
           </p>
 
           <div className="flex flex-row items-center justify-center gap-4 md:justify-start">
