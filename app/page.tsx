3e62f955efc68aee0f62d3c8818e4cfd93af0fcb
Andrei <ierdss@proton.me>
2025-08-18 22:55:40 +0800

    feat(page): remove scrollspy

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,14 @@
 "use client";
 
 import { Experience, Hero, Projects, Skills } from "@/components";
-import ScrollSpy from "react-ui-scrollspy";
 
 export default function Home() {
   return (
-    <ScrollSpy
-      scrollThrottle={300}
-      offsetBottom={100}
-      updateHistoryStack={true}
-      useBoxMethod={true}
-    >
-      <main className="flex w-full flex-col items-center justify-center">
-        <Hero />
-        <Skills />
-        <Experience />
-        <Projects />
-      </main>
-    </ScrollSpy>
+    <main className="flex w-full flex-col items-center justify-center">
+      <Hero />
+      <Skills />
+      <Experience />
+      <Projects />
+    </main>
   );
 }
