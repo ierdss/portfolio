0107e30dfa49e57e35d9f32e76254d82c4c9d914
Andrei <ierdss@proton.me>
2025-08-28 21:31:57 +0800

    style(404): add bg pattern

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="bg-gray-100 flex min-h-screen flex-col items-center justify-center">
-      <h2>404 Not Found</h2>
+    <div className="flex h-screen min-h-screen flex-col items-center justify-center bg-pattern">
+      <h2 className="text-redberry">404 Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
     </div>
