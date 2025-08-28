5d4dc60a45ee766d475b87432181a45e00da13ce
Andrei <ierdss@proton.me>
2025-08-28 21:45:27 +0800

    style(footer): remove padding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="relative flex w-full flex-col items-center justify-center gap-12 bg-background-1  md:flex-row"
+      className="relative flex w-full flex-col items-center justify-center gap-12 bg-background-1 md:flex-row"
     >
-      <div className="flex w-full max-w-center justify-center px-2 py-12 md:px-40 md:py-24">
+      <div className="flex w-full max-w-center justify-center">
         <p className="flex flex-row gap-[6px] text-xs text-text md:order-2 md:text-xl">
           © 2023{" "}
           <span className="flex h-fit w-fit font-bold"> Andrei Sager</span>
