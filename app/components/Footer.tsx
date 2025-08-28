a7bce465be910e75c0d6c870347d5d2dcd9e99d2
Andrei <ierdss@proton.me>
2025-08-28 21:58:12 +0800

    style(footer): add padding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       id="footer"
       className="absolute bottom-0 flex h-16 w-full max-w-center flex-col items-center justify-center bg-transparent"
     >
-      <p className="flex flex-row gap-2 text-text">
+      <p className="flex flex-row gap-2 px-4 text-center text-text">
         © 2023 Andrei Sager. All rights reserved.
       </p>
     </footer>
