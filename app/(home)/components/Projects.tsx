a1c890a9db48b202548caac34fab8bdc6a2919cc
Andrei <ierdss@proton.me>
2025-08-28 23:13:19 +0800

    style(projects): add magnetic wrappers

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -2,7 +2,9 @@ import { Heading1 } from "@/components";
 import ViewMoreBtn from "@/components/buttons/ViewMoreBtn";
 import Divider from "@/components/divider/Divider";
 import { ProjectsDataFeatured } from "@/constants";
+import Magnetic from "@/wrappers/MagneticWrapper";
 import Image from "next/image";
+import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
 import { TbBrandGithubFilled, TbExternalLink } from "react-icons/tb";
 
@@ -132,32 +134,44 @@ function FeaturedProjectsCard({
         </ul>
         <div className="flex w-full flex-row flex-wrap items-center gap-4">
           {linkLiveDemo && (
-            <a
-              href={linkLiveDemo}
-              target="_blank"
-              className="text-primary-foreground shadow-xs [&_svg:not([class*='size-'])]:size-4 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive has-[>svg]:px-3 inline-flex h-9 shrink-0 select-none items-center justify-center gap-2 whitespace-nowrap rounded-full bg-text px-4 py-2 text-sm font-medium text-background-1 outline-none transition-all hover:bg-primary/90 focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0"
-            >
-              <TbExternalLink size={size} />
-              <p>Live Demo</p>
-            </a>
+            <Magnetic>
+              <div>
+                <Link
+                  href={linkLiveDemo}
+                  target="_blank"
+                  className="text-primary-foreground shadow-xs [&_svg:not([class*='size-'])]:size-4 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive has-[>svg]:px-3 inline-flex h-9 shrink-0 select-none items-center justify-center gap-2 whitespace-nowrap rounded-full bg-text px-4 py-2 text-sm font-medium text-background-1 outline-none transition-all hover:bg-primary/90 focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0"
+                >
+                  <TbExternalLink size={size} />
+                  <p>Live Demo</p>
+                </Link>
+              </div>
+            </Magnetic>
           )}
           {linkRepository && (
-            <a
-              href={linkRepository}
-              target="_blank"
-              className="rounded-full bg-text p-2 text-background-1 hover:bg-primary"
-            >
-              <TbBrandGithubFilled size={size} />
-            </a>
+            <Magnetic>
+              <div>
+                <Link
+                  href={linkRepository}
+                  target="_blank"
+                  className="flex rounded-full bg-text p-2 text-background-1 hover:bg-primary"
+                >
+                  <TbBrandGithubFilled size={size} />
+                </Link>
+              </div>
+            </Magnetic>
           )}
           {linkCaseStudy && (
-            <a
-              href={linkCaseStudy}
-              target="_blank"
-              className="rounded-full bg-text p-2 text-background-1 hover:bg-primary"
-            >
-              <FaSearch size={size} />
-            </a>
+            <Magnetic>
+              <div>
+                <Link
+                  href={linkCaseStudy}
+                  target="_blank"
+                  className="flex rounded-full bg-text p-2 text-background-1 hover:bg-primary"
+                >
+                  <FaSearch size={size} />
+                </Link>
+              </div>
+            </Magnetic>
           )}
         </div>
       </div>
