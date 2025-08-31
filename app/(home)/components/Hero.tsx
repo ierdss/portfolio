53718f2400248156c9d1b1071a96b0b452ea5610
Andrei <ierdss@proton.me>
2025-09-01 01:07:13 +0800

    feat(hero): add sunglasses to show i'm a chill guy

--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -47,39 +47,45 @@ export default function Hero() {
       <div className="flex w-full max-w-center flex-col items-center justify-center gap-10 px-2 py-24 md:flex-row lg:gap-20 lg:py-48">
         <div className="z-10 flex w-full max-w-[300px] items-center justify-center overflow-hidden rounded-full bg-background-1/50 lg:max-w-[400px]">
           {theme === "light" && (
-            <Image
-              src="/portraits/barong_light.png"
-              width={1000}
-              height={1000}
-              alt="Just a guy in barong."
-              priority
-            />
+            <div className="group">
+              <Image
+                src="/portraits/barong_light.png"
+                width={1000}
+                height={1000}
+                alt="Just a guy in barong."
+                priority
+                className="flex group-hover:hidden"
+              />
+              <Image
+                src="/portraits/barong_light_chill.png"
+                width={1000}
+                height={1000}
+                alt="Just a chill guy in barong."
+                priority
+                className="hidden transition-all duration-300 ease-in-out group-hover:block"
+              />
+            </div>
           )}
-          {/* <Image
-              src="/portraits/barong_light_chill.png"
-              width={1000}
-              height={1000}
-              alt="Just a chill guy in barong."
-              priority
-              className="hidden transition-all duration-300 ease-in-out group-hover:flex"
-            /> */}
           {theme === "dark" && (
-            <Image
-              src="/portraits/barong_dark.png"
-              width={1000}
-              height={1000}
-              alt="Just a guy in barong."
-              priority
-            />
+            <div className="group">
+              <Image
+                src="/portraits/barong_dark.png"
+                width={1000}
+                height={1000}
+                alt="Just a guy in barong."
+                priority
+                className="flex group-hover:hidden"
+              />
+              <Image
+                src="/portraits/barong_dark_chill.png"
+                width={1000}
+                height={1000}
+                alt="Just a chill guy in barong."
+                priority
+                className="hidden transition-all duration-300 ease-in-out group-hover:block"
+              />
+            </div>
           )}
-          {/* <Image
-            src="/portraits/barong_dark_chill.png"
-            width={1000}
-            height={1000}
-            alt="Just a chill guy in barong."
-            priority
-            className="hidden transition-all duration-300 ease-in-out group-hover:block dark:group-hover:hidden"
-          /> */}
         </div>
 
         <div className="z-20 order-2 flex w-fit max-w-[500px] shrink-0 flex-col items-center justify-center gap-4 text-center md:max-w-[350px] md:items-start md:gap-6 md:text-left lg:max-w-[500px]">
