47794d047526406eecb3a084b7bb3205241eae62
Andrei <ierdss@proton.me>
2025-09-01 21:43:38 +0800

    feat(footer): fix alignment

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,11 +2,13 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="absolute bottom-0 flex h-16 w-full max-w-center flex-col items-center justify-center bg-transparent"
+      className="absolute bottom-0 flex w-full items-center justify-center"
     >
-      <p className="flex flex-row gap-2 px-4 text-center text-text">
-        © 2023 Andrei Sager. All rights reserved.
-      </p>
+      <div className=" flex h-16 w-full max-w-center flex-col items-center justify-center bg-transparent">
+        <p className="flex flex-row gap-2 px-4 text-center text-text">
+          © 2023 Andrei Sager. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
