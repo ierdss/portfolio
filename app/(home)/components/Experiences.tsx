886c7fc8349a5a4c38126833666ea8fdfbb37c87
Andrei <ierdss@proton.me>
2025-09-01 23:06:54 +0800

    feat(exp): add gsap hover animation

--- app/(home)/components/Experiences.tsx
+++ app/(home)/components/Experiences.tsx
@@ -35,6 +35,27 @@ export default function Experiences() {
         gsap.set(batch, { opacity: 0, y: 50, overwrite: true }),
     });
   }, [tab]);
+
+  useGSAP(() => {
+    // Hover Animation
+    gsap.utils.toArray(".exp-hover").forEach((el) => {
+      const element = el as Element;
+      element.addEventListener("mouseenter", () => {
+        gsap.to(element, {
+          scale: 0.9,
+          ease: "back.out(3)",
+          overwrite: true,
+        });
+      });
+      element.addEventListener("mouseleave", () => {
+        gsap.to(element, {
+          scale: 1,
+          ease: "back.out(3)",
+          overwrite: true,
+        });
+      });
+    });
+  }, [tab]);
   return (
     <div
       id="experience"
@@ -80,7 +101,7 @@ export default function Experiences() {
                 bullets,
                 tags,
               }) => (
-                <li className="exp-card flex flex-col gap-4" key={id}>
+                <li className="exp-card exp-hover flex flex-col gap-4" key={id}>
                   <div className="flex flex-col justify-between lg:flex-row lg:items-center">
                     <h5 className="text-lg">
                       {title} •<span className="text-redberry"> {company}</span>
@@ -116,7 +137,7 @@ export default function Experiences() {
           <ul className="flex w-full max-w-screen-xl flex-col gap-8">
             {education.map(
               ({ id, degree, institution, graduationDate, description }) => (
-                <li className="exp-card flex flex-col gap-4" key={id}>
+                <li className="exp-card exp-hover flex flex-col gap-4" key={id}>
                   <div className="flex flex-col justify-between lg:flex-row lg:items-center">
                     <h5 className="text-lg">
                       {degree} •
@@ -136,7 +157,7 @@ export default function Experiences() {
         {tab === "Upskill" && (
           <ul className="flex w-full max-w-screen-xl flex-col gap-8">
             {upskill.map(({ id, title, platform, date, tags }) => (
-              <li className="exp-card flex flex-col gap-4 " key={id}>
+              <li className="exp-card exp-hover flex flex-col gap-4 " key={id}>
                 <div className="flex flex-col justify-between lg:flex-row lg:items-center">
                   <h5 className="text-lg">
                     {title} •<span className="text-redberry"> {platform}</span>
