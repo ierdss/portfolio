e8e66dee6a06edd203a822ac6f7805793985e594
Andrei <ierdss@proton.me>
2026-02-13 19:24:13 +0800

    data(hero): update specialty

--- app/constants/HeroData.tsx
+++ app/constants/HeroData.tsx
@@ -2,5 +2,7 @@ export const HeroData = {
   name: "Andrei",
   title: "Frontend Web Developer",
   location: "Based in the Philippines 🇵🇭🍌",
-  specialty: "Full-Scale Websites & Interactive Demonstration Tools",
+  specialty: "Full-Scale Websites & Utility Tools",
 };
+
+// For the future: Interactive Demonstration Tools
